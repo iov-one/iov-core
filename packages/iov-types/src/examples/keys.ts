faf839d4473febbce2673f6ece85c8882ded63e6
will-iov <will-iov@users.noreply.github.com>
2018-04-21 16:23:50 +0200

    iov-types: Format source code

--- packages/iov-types/src/examples/keys.ts
+++ packages/iov-types/src/examples/keys.ts
@@ -1,5 +1,11 @@
-export const publicKeyString: PublicKeyString = 'abcd1234';
-export const publicKeyBuffer: PublicKeyBuffer = Buffer.from(publicKeyString, 'hex');
+export const publicKeyString: PublicKeyString = "abcd1234";
+export const publicKeyBuffer: PublicKeyBuffer = Buffer.from(
+  publicKeyString,
+  "hex"
+);
 
-export const privateKeyString: PrivateKeyString = 'abcd1234';
-export const privateKeyBuffer: PrivateKeyBuffer = Buffer.from(privateKeyString, 'hex');
+export const privateKeyString: PrivateKeyString = "abcd1234";
+export const privateKeyBuffer: PrivateKeyBuffer = Buffer.from(
+  privateKeyString,
+  "hex"
+);
