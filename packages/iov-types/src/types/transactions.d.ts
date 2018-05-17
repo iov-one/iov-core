0d45a48ca1c5c01e1930bf325cab39f1364dd39b
will-iov <will-iov@users.noreply.github.com>
2018-05-17 13:42:20 +0200

    types: Add nonce type

--- packages/iov-types/src/types/transactions.d.ts
+++ packages/iov-types/src/types/transactions.d.ts
@@ -2,6 +2,11 @@ import {
   PublicKeyString,
 } from './keys'
 
+declare const NonceSymbol: unique symbol;
+type Nonce = typeof NonceSymbol;
+export type NonceBuffer = Nonce & Uint8Array;
+export type NonceString = Nonce & string;
+
 export interface Transaction {
   readonly amount: number,
   readonly kind: string,
