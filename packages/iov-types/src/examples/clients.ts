32d599cc04fe67e40269d351ac49bb050e709c6c
will-iov <will-iov@users.noreply.github.com>
2018-05-17 17:23:56 +0200

    types: Format code

--- packages/iov-types/src/examples/clients.ts
+++ packages/iov-types/src/examples/clients.ts
@@ -1,8 +1,5 @@
-import {
-  ClientNameString,
-  ClientTokenString
-} from '../types/clients'
+import { ClientNameString, ClientTokenString } from "../types/clients";
 
-export const clientName: ClientNameString = 'my.web4client.com' as ClientNameString
+export const clientName: ClientNameString = "my.web4client.com" as ClientNameString;
 
-export const clientToken: ClientTokenString = 'abcd1234' as ClientTokenString
+export const clientToken: ClientTokenString = "abcd1234" as ClientTokenString;
--- packages/iov-types/src/types/clients.d.ts
+++ packages/iov-types/src/types/clients.d.ts
@@ -1,7 +1,7 @@
 declare const ClientNameSymbol: unique symbol;
-type ClientName = typeof ClientNameSymbol
-export type ClientNameString = ClientName & string
+type ClientName = typeof ClientNameSymbol;
+export type ClientNameString = ClientName & string;
 
 declare const ClientTokenSymbol: unique symbol;
-type ClientToken = typeof ClientTokenSymbol
-export type ClientTokenString = ClientToken & string
+type ClientToken = typeof ClientTokenSymbol;
+export type ClientTokenString = ClientToken & string;
