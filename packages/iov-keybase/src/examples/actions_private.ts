efad39fd4766cedc81674f743a631b350392fc57
will-iov <will-iov@users.noreply.github.com>
2018-05-17 18:44:28 +0200

    keybase: Split public and private actions into separate files

--- packages/iov-keybase/src/examples/actions_private.ts
+++ packages/iov-keybase/src/examples/actions_private.ts
@@ -10,23 +10,15 @@ import {
 import { PasswordString, UsernameString } from "../types/accounts";
 import {
   AddAccount,
-  ChangeEvent,
   CreateUser,
   DecryptMessage,
   EncryptMessage,
   ExportUser,
-  GetCurrentAccount,
   GrantStoreAccess,
   ImportPrivateKey,
   ListUsers,
   PrivateAction,
   PrivateActionType,
-  PublicAction,
-  PublicActionType,
-  PublicEventType,
-  RequestAPIAccess,
-  RequestSignMessage,
-  RequestSignTransaction,
   RestoreUser,
   SetActiveKey,
   SignMessage,
@@ -34,45 +26,7 @@ import {
   SubmitPassword,
   VerifyMessage,
   VerifyTransaction
-} from "../types/actions";
-
-export const requestAPIAccessAction: RequestAPIAccess = {
-  type: PublicActionType.REQUEST_API_ACCESS
-};
-
-export const requestAPIAccessActionWithOptions: RequestAPIAccess = {
-  options: {
-    some: "future option"
-  },
-  type: PublicActionType.REQUEST_API_ACCESS
-};
-
-export const getCurrentAccountAction: GetCurrentAccount = {
-  type: PublicActionType.GET_CURRENT_ACCOUNT
-};
-
-export const changeEventAction: ChangeEvent = {
-  event: PublicEventType.ACCESS_GRANTED,
-  handler: () => "We were given access",
-  type: PublicActionType.CHANGE_EVENT
-};
-
-export const requestSignTransactionAction: RequestSignTransaction = {
-  transaction: {
-    amount: 123,
-    kind: "send",
-    sender: "0350863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352" as PublicKeyString
-  },
-  type: PublicActionType.REQUEST_SIGN_TX
-};
-
-export const requestSignMessageAction: RequestSignMessage = {
-  message: new Uint8Array([10, 20, 30]),
-  type: PublicActionType.REQUEST_SIGN_MESSAGE
-};
-
-export const handlePublicAction = (action: PublicAction) => action;
-export const handledPublicAction = handlePublicAction(requestAPIAccessAction);
+} from "../types/actions_private";
 
 export const listUsersAction: ListUsers = {
   type: PrivateActionType.LIST_USERS
--- packages/iov-keybase/src/types/actions_private.d.ts
+++ packages/iov-keybase/src/types/actions_private.d.ts
@@ -10,54 +10,6 @@ import {
 } from "@iov/types";
 import { PasswordString, UsernameString } from "./accounts";
 
-export const enum PublicActionType {
-  REQUEST_API_ACCESS = "REQUEST_API_ACCESS",
-  GET_CURRENT_ACCOUNT = "GET_CURRENT_ACCOUNT",
-  CHANGE_EVENT = "CHANGE_EVENT",
-  REQUEST_SIGN_TX = "REQUEST_SIGN_TX",
-  REQUEST_SIGN_MESSAGE = "REQUEST_SIGN_MESSAGE"
-}
-
-export interface RequestAPIAccess {
-  readonly type: PublicActionType.REQUEST_API_ACCESS;
-  readonly options?: {};
-}
-
-export interface GetCurrentAccount {
-  readonly type: PublicActionType.GET_CURRENT_ACCOUNT;
-}
-
-export const enum PublicEventType {
-  ACCESS_GRANTED = "ACCESS_GRANTED",
-  PUBLIC_KEY_RELEASED = "PUBLIC_KEY_RELEASED",
-  WALLET_LOCKED = "WALLET_LOCKED"
-}
-
-export interface ChangeEvent {
-  readonly type: PublicActionType.CHANGE_EVENT;
-  readonly event: PublicEventType;
-  // no-mixed-interface protects against OO-style methods, but this is passing a handler
-  // tslint:disable-next-line no-mixed-interface
-  readonly handler: () => any;
-}
-
-export interface RequestSignTransaction {
-  readonly type: PublicActionType.REQUEST_SIGN_TX;
-  readonly transaction: Transaction;
-}
-
-export interface RequestSignMessage {
-  readonly type: PublicActionType.REQUEST_SIGN_MESSAGE;
-  readonly message: Uint8Array;
-}
-
-export type PublicAction =
-  | RequestAPIAccess
-  | GetCurrentAccount
-  | ChangeEvent
-  | RequestSignTransaction
-  | RequestSignMessage;
-
 export const enum PrivateActionType {
   LIST_USERS = "LIST_USERS",
   SUBMIT_PASSWORD = "SUBMIT_PASSWORD",
