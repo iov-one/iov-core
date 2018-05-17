12da1e224cdd33a5419529d6ceb83a0b660259ce
will-iov <will-iov@users.noreply.github.com>
2018-05-17 16:18:43 +0200

    keybase: Add accounts types: username & password

---
+++ packages/iov-keybase/src/examples/accounts.ts
@@ -0,0 +1,8 @@
+import {
+  PasswordString,
+  UsernameString
+} from '../types/accounts'
+
+export const password: PasswordString = 'password123' as PasswordString
+
+export const username: UsernameString = 'my_username' as UsernameString
---
+++ packages/iov-keybase/src/types/accounts.d.ts
@@ -0,0 +1,7 @@
+declare const UsernameSymbol: unique symbol;
+type Username = typeof UsernameSymbol
+export type UsernameString = Username & string
+
+declare const PasswordSymbol: unique symbol;
+type Password = typeof PasswordSymbol
+export type PasswordString = Password & string
