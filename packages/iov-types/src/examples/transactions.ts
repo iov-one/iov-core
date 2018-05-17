b5c5522038c1657200acfcb0505c721da80f6ab9
will-iov <will-iov@users.noreply.github.com>
2018-05-17 16:09:39 +0200

    types: Add nonce and ttl type examples

--- packages/iov-types/src/examples/transactions.ts
+++ packages/iov-types/src/examples/transactions.ts
@@ -2,11 +2,21 @@ import {
   PublicKeyString,
 } from '../types/keys'
 import {
+  NonceBuffer,
+  NonceString,
   Transaction,
+  TTLBuffer,
+  TTLString
 } from '../types/transactions';
+
+export const nonceBuffer: NonceBuffer = new Uint8Array([0, 0, 0, 12]) as NonceBuffer
+export const nonceString: NonceString = '12' as NonceString;
 
 export const transaction: Transaction = {
   amount: 123,
   kind: 'send',
   sender: '0350863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352' as PublicKeyString,
 }
+
+export const ttlBuffer: TTLBuffer = new Uint8Array([0, 2, 0, 0]) as TTLBuffer
+export const ttlString: TTLString = '1000' as TTLString;
