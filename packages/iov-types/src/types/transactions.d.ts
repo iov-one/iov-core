48cda709f6d97a0b7e157bb02d01b2effd0e61ea
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-05-30 17:00:22 +0200

    Start defining action tx types

--- packages/iov-types/src/types/transactions.d.ts
+++ packages/iov-types/src/types/transactions.d.ts
@@ -10,16 +10,83 @@ type TransactionID = typeof TransactionIDSymbol;
 export type TransactionIDBuffer = TransactionID & Uint8Array;
 export type TransactionIDString = TransactionID & string;
 
+// TODO: can't we just make this a number (block height?)
 declare const TTLSymbol: unique symbol;
 type TTL = typeof TTLSymbol;
 export type TTLBuffer = TTL & Uint8Array;
 export type TTLString = TTL & string;
 
-// TODO: Transaction needs clear definition... talk with Isabella as well
-export interface Transaction {
-  readonly amount: number;
-  readonly kind: string;
-  readonly sender: PublicKeyString;
+// CurrencyCode should be 3-4 letters, uppercase
+declare const CurrencySymbol: unique symbol;
+export type CurrencyCode = typeof CurrencySymbol & string;
+
+// ChainID should be 3-4 letters, uppercase
+declare const ChainSymbol: unique symbol;
+export type ChainID = typeof ChainSymbol & string;
+
+declare const SwapIDSymbol: unique symbol;
+type SwapID = typeof SwapIDSymbol;
+export type SwapIDBuffer = SwapID & Uint8Array;
+export type SwapIDString = SwapID & string;
+
+export interface Coin {
+  readonly whole: number;
+  readonly fractional?: number;
+  readonly currency: CurrencyCode;
 }
 
+export interface BaseTx {
+  readonly chainId: ChainID;
+  readonly fee: Coin;
+  readonly signer: PublicKeyString;
+  readonly ttl?: TTLString;
+}
+
+export interface SendTx extends BaseTx {
+  readonly kind: "send";
+  readonly amount: Coin;
+  readonly recipient: PublicKeyString;
+}
+
+export interface SetNameTx extends BaseTx {
+  readonly kind: "set_name";
+  readonly name: string;
+}
+
+export interface SwapOfferTx extends BaseTx {
+  readonly kind: "swap_offer";
+  readonly amount: Coin;
+  readonly recipient: PublicKeyString;
+  readonly timeout: number; // number of blocks in the future
+  readonly preimage: Uint8Array;
+}
+
+export interface SwapCounterTx extends BaseTx {
+  readonly kind: "swap_counter";
+  readonly amount: Coin;
+  readonly recipient: PublicKeyString;
+  readonly timeout: number; // number of blocks in the future
+  readonly hash: Uint8Array;
+}
+
+export interface SwapClaimTx extends BaseTx {
+  readonly kind: "swap_claim";
+  readonly preimage: Uint8Array;
+  readonly swapId: SwapIDString;
+}
+
+export interface SwapTimeoutTx extends BaseTx {
+  readonly kind: "swap_timeout";
+  readonly swapId: SwapIDString;
+}
+
+export type Transaction =
+  | SendTx
+  | SetNameTx
+  | SwapOfferTx
+  | SwapCounterTx
+  | SwapClaimTx
+  | SwapTimeoutTx;
+
+// TODO: Identifier is chain-dependent.. maybe rethink this as generic function
 export type Identifier = (tx: Transaction) => TransactionIDString;
