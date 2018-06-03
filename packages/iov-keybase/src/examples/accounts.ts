465ebff5d75a5801698c213bc7183b68e37d2a1b
will-iov <will-iov@users.noreply.github.com>
2018-06-03 17:42:59 +0200

    keybase: Add placeholder Account type

--- packages/iov-keybase/src/examples/accounts.ts
+++ packages/iov-keybase/src/examples/accounts.ts
@@ -1,5 +1,10 @@
-import { PasswordString, UsernameString } from "../types/accounts";
+import { PublicKeyString } from "@iov/types";
+import { Account, PasswordString, UsernameString } from "../types/accounts";
 
 export const password: PasswordString = "password123" as PasswordString;
 
 export const username: UsernameString = "my_username" as UsernameString;
+
+export const account: Account = {
+  publicKey: "a5bdf5841d9c56d6d975c1ab56ba569c3e367aafa2f9e2ce3dc518eab2594b77" as PublicKeyString
+};
--- packages/iov-keybase/src/types/accounts.d.ts
+++ packages/iov-keybase/src/types/accounts.d.ts
@@ -1,3 +1,5 @@
+import { PublicKeyString } from "@iov/types";
+
 declare const UsernameSymbol: unique symbol;
 type Username = typeof UsernameSymbol;
 export type UsernameString = Username & string;
@@ -5,3 +7,7 @@ export type UsernameString = Username & string;
 declare const PasswordSymbol: unique symbol;
 type Password = typeof PasswordSymbol;
 export type PasswordString = Password & string;
+
+export interface Account {
+  readonly publicKey: PublicKeyString;
+}
