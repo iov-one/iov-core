edaaa61d9a53b7476142616f23ef8e317c00438e
Simon Warta <simon@kullo.net>
2018-06-05 16:05:56 +0200

    Workaround libsodium-wrapper bug

--- packages/testable-demo/src/crypto.ts
+++ packages/testable-demo/src/crypto.ts
@@ -1,4 +1,6 @@
-import * as sodium from "libsodium-wrappers";
+// use require instead of import because of this bug
+// https://github.com/jedisct1/libsodium.js/issues/148
+import sodium = require("libsodium-wrappers");
 
 export class Ed25519 {
   static async generateKeypair(): Promise<any> {
