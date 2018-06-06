3420304f6a2bac0eb19e17c98d005794821c8848
Simon Warta <simon@kullo.net>
2018-06-06 11:24:05 +0200

    Add link to libsodium.js API

--- packages/testable-demo/src/crypto.ts
+++ packages/testable-demo/src/crypto.ts
@@ -1,3 +1,5 @@
+// libsodium.js API: https://gist.github.com/webmaster128/b2dbe6d54d36dd168c9fabf441b9b09c
+
 // use require instead of import because of this bug
 // https://github.com/jedisct1/libsodium.js/issues/148
 import sodium = require("libsodium-wrappers");
