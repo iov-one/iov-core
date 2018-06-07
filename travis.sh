8f9f9399e60ae923af5ef886a8cb35c69e31923a
Simon Warta <simon@kullo.net>
2018-06-07 16:20:55 +0200

    Build and run default tests on all OSs

--- travis.sh
+++ travis.sh
@@ -2,15 +2,16 @@
 set -o errexit -o nounset -o pipefail
 which shellcheck > /dev/null && shellcheck "$0"
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    # Test browser
-    (
-      cd packages/iov-crypto
+yarn build
+yarn test
+
+(
+  # Test browser
+  cd packages/iov-crypto
+
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
       yarn test-safari
       yarn test-firefox
       yarn test-chrome
-    )
-else
-    yarn build
-    yarn test
-fi
+  fi
+)
