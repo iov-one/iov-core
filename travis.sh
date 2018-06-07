3538c9eb6a41aa31e3bd77cf4b786a44923ec17c
Simon Warta <simon@kullo.net>
2018-06-07 16:26:13 +0200

    Test firefox on Linux as well

--- travis.sh
+++ travis.sh
@@ -9,9 +9,10 @@ yarn test
   # Test browser
   cd packages/iov-crypto
 
+  yarn test-firefox
+
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
       yarn test-safari
-      yarn test-firefox
       yarn test-chrome
   fi
 )
