33bd28359f84864af36122f000280b9684ad20ac
Simon Warta <simon@kullo.net>
2018-06-12 10:54:04 +0200

    Ensure build step didn't modify source files

--- travis.sh
+++ travis.sh
@@ -3,6 +3,17 @@ set -o errexit -o nounset -o pipefail
 which shellcheck > /dev/null && shellcheck "$0"
 
 yarn build
+
+# Ensure build step didn't modify source files to avoid unprettified repository state
+SOURCE_CHANGES=$(git status --porcelain)
+if [[ -n "$SOURCE_CHANGES" ]]; then
+  echo "Error: repository contains changes."
+  echo "Showing 'git status' and 'git diff' for debugging reasons now:"
+  git status
+  git diff
+  exit 1
+fi
+
 yarn test
 
 (
