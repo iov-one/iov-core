12718c83ca1df609b9d619ca3391cb378efe3456
Simon Warta <simon@kullo.net>
2018-06-12 10:56:33 +0200

    Fix indentation in travis.sh

--- travis.sh
+++ travis.sh
@@ -23,9 +23,9 @@ yarn test
   yarn test-chrome
 
   if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-      yarn test-safari
+    yarn test-safari
 
-      # Firefox does not run on Linux VMs because "no DISPLAY environment variable specified"
-      yarn test-firefox
+    # Firefox does not run on Linux VMs because "no DISPLAY environment variable specified"
+    yarn test-firefox
   fi
 )
