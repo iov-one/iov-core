bff60d29a2e8c324c503f732081ca3fe2ec6cb84
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-06-12 19:55:32 +0200

    Pull out encode/build logic into own file

---
+++ packages/bns-codec/src/encode.ts
@@ -0,0 +1,86 @@
+import {
+  FullSignature,
+  SendTx,
+  SetNameTx,
+  SwapClaimTx,
+  SwapCounterTx,
+  SwapOfferTx,
+  SwapTimeoutTx,
+  Transaction,
+  TransactionKind,
+} from "@iov/types";
+import * as codec from "./codec";
+import { encodeFullSig, encodeToken } from "./types";
+import { hashIdentifier, hashPreimage, keyToAddress, keyToIdentifier } from "./util";
+
+export const buildTx = async (
+  tx: Transaction,
+  sigs: ReadonlyArray<FullSignature>,
+): Promise<codec.app.ITx> => {
+  const msg = await buildMsg(tx);
+  return codec.app.Tx.create({
+    ...msg,
+    fees: { fees: encodeToken(tx.fee) },
+    signatures: sigs.map(encodeFullSig),
+  });
+};
+
+const buildMsg = (tx: Transaction): Promise<codec.app.ITx> => {
+  switch (tx.kind) {
+    case TransactionKind.SEND:
+      return buildSendTx(tx);
+    case TransactionKind.SET_NAME:
+      return buildSetNameTx(tx);
+    case TransactionKind.SWAP_OFFER:
+      return buildSwapOfferTx(tx);
+    case TransactionKind.SWAP_COUNTER:
+      return buildSwapCounterTx(tx);
+    case TransactionKind.SWAP_CLAIM:
+      return buildSwapClaimTx(tx);
+    case TransactionKind.SWAP_TIMEOUT:
+      return buildSwapTimeoutTx(tx);
+  }
+};
+
+const buildSendTx = async (tx: SendTx): Promise<codec.app.ITx> => ({
+  sendMsg: codec.cash.SendMsg.create({
+    src: await keyToAddress(tx.signer),
+    dest: await keyToAddress(tx.recipient),
+    amount: encodeToken(tx.amount),
+  }),
+});
+
+const buildSetNameTx = async (tx: SetNameTx): Promise<codec.app.ITx> => ({
+  setNameMsg: codec.namecoin.SetWalletNameMsg.create({
+    address: await keyToAddress(tx.signer),
+    name: tx.name,
+  }),
+});
+
+const buildSwapOfferTx = async (tx: SwapOfferTx): Promise<codec.app.ITx> => {
+  const hashed = { ...tx, hash: await hashPreimage(tx.preimage), kind: TransactionKind.SWAP_COUNTER };
+  return buildSwapCounterTx(hashed as SwapCounterTx);
+};
+
+const buildSwapCounterTx = async (tx: SwapCounterTx): Promise<codec.app.ITx> => ({
+  createEscrowMsg: codec.escrow.CreateEscrowMsg.create({
+    sender: await keyToIdentifier(tx.signer),
+    arbiter: hashIdentifier(tx.hash),
+    recipient: await keyToIdentifier(tx.recipient),
+    timeout: tx.timeout,
+    amount: tx.amount.map(encodeToken),
+  }),
+});
+
+const buildSwapClaimTx = async (tx: SwapClaimTx): Promise<codec.app.ITx> => ({
+  releaseEscrowMsg: codec.escrow.ReleaseEscrowMsg.create({
+    escrowId: tx.swapId,
+  }),
+  preimage: tx.preimage,
+});
+
+const buildSwapTimeoutTx = async (tx: SwapTimeoutTx): Promise<codec.app.ITx> => ({
+  returnEscrowMsg: codec.escrow.ReturnEscrowMsg.create({
+    escrowId: tx.swapId,
+  }),
+});
