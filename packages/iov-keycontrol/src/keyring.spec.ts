3b119d3c6e4734d692f56cfe698c9c95933f286e
Simon Warta <simon@kullo.net>
2018-06-12 16:55:22 +0200

    Add create identity tests

--- packages/iov-keycontrol/src/keyring.spec.ts
+++ packages/iov-keycontrol/src/keyring.spec.ts
@@ -16,5 +16,47 @@ describe("Keyring", () => {
         done();
       })();
     });
+
+    it("can create an identity", done => {
+      (async () => {
+        const keyringEntry = new Ed25519KeyringEntry();
+        const newIdentity = await keyringEntry.createIdentity();
+        expect((await keyringEntry.getIdentities()).length).toEqual(1);
+
+        const firstIdentity = (await keyringEntry.getIdentities())[0];
+        expect(newIdentity.algo).toEqual(firstIdentity.algo);
+        expect(newIdentity.data).toEqual(firstIdentity.data);
+        expect(newIdentity.nickname).toEqual(firstIdentity.nickname);
+        expect(newIdentity.canSign).toEqual(firstIdentity.canSign);
+
+        done();
+      })();
+    });
+
+    it("can create multiple identities", done => {
+      (async () => {
+        const keyringEntry = new Ed25519KeyringEntry();
+        const newIdentity1 = await keyringEntry.createIdentity(); // 1
+        await keyringEntry.createIdentity(); // 2
+        await keyringEntry.createIdentity(); // 3
+        await keyringEntry.createIdentity(); // 4
+        const newIdentity5 = await keyringEntry.createIdentity(); // 5
+        expect((await keyringEntry.getIdentities()).length).toEqual(5);
+
+        const firstIdentity = (await keyringEntry.getIdentities())[0];
+        expect(newIdentity1.algo).toEqual(firstIdentity.algo);
+        expect(newIdentity1.data).toEqual(firstIdentity.data);
+        expect(newIdentity1.nickname).toEqual(firstIdentity.nickname);
+        expect(newIdentity1.canSign).toEqual(firstIdentity.canSign);
+
+        const lastIdentity = (await keyringEntry.getIdentities())[4];
+        expect(newIdentity5.algo).toEqual(lastIdentity.algo);
+        expect(newIdentity5.data).toEqual(lastIdentity.data);
+        expect(newIdentity5.nickname).toEqual(lastIdentity.nickname);
+        expect(newIdentity5.canSign).toEqual(lastIdentity.canSign);
+
+        done();
+      })();
+    });
   });
 });
