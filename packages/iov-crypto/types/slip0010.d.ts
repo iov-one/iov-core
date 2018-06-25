3ecf6e607c18296b6a0616907bb565c8c38fcda9
Simon Warta <simon@kullo.net>
2018-06-25 10:58:46 +0200

    Create strict type Slip0010RawIndex

--- packages/iov-crypto/types/slip0010.d.ts
+++ packages/iov-crypto/types/slip0010.d.ts
@@ -1,3 +1,4 @@
+import { Uint32 } from "./utils";
 export interface Slip0010Result {
     readonly chainCode: Uint8Array;
     readonly privkey: Uint8Array;
@@ -6,10 +7,13 @@ export declare enum Slip0010Curve {
     Secp256k1 = "Bitcoin seed",
     Ed25519 = "ed25519 seed",
 }
+export declare class Slip0010RawIndex extends Uint32 {
+    isHardened(): boolean;
+}
 export declare class Slip0010 {
-    static derivePath(curve: Slip0010Curve, seed: Uint8Array, path: ReadonlyArray<number>): Slip0010Result;
-    static rawIndexFromHardened(hardenedIndex: number): number;
-    static rawIndexFromNormal(normalIndex: number): number;
+    static derivePath(curve: Slip0010Curve, seed: Uint8Array, path: ReadonlyArray<Slip0010RawIndex>): Slip0010Result;
+    static rawIndexFromHardened(hardenedIndex: number): Slip0010RawIndex;
+    static rawIndexFromNormal(normalIndex: number): Slip0010RawIndex;
     private static master(curve, seed);
     private static child(curve, parentPrivkey, parentChainCode, rawIndex);
     private static childImpl(curve, parentPrivkey, parentChainCode, rawIndex, i);
