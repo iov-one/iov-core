642218ad12257262aff4c5c747fa0ffd2cdaa4e5
Simon Warta <simon@kullo.net>
2018-06-25 13:14:07 +0200

    Test Ed25519 secret to public conversion

--- packages/iov-crypto/src/libsodium.spec.ts
+++ packages/iov-crypto/src/libsodium.spec.ts
@@ -63,6 +63,25 @@ describe("Libsodium", () => {
         }
 
         {
+          // Test secret to public conversion (TEST 1–4 from https://tools.ietf.org/html/rfc8032#section-7.1)
+          const privkey1 = fromHex("9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60");
+          const pubkey1 = (await Ed25519.makeKeypair(privkey1)).pubkey;
+          expect(pubkey1).toEqual(fromHex("d75a980182b10ab7d54bfed3c964073a0ee172f3daa62325af021a68f707511a"));
+
+          const privkey2 = fromHex("4ccd089b28ff96da9db6c346ec114e0f5b8a319f35aba624da8cf6ed4fb8a6fb");
+          const pubkey2 = (await Ed25519.makeKeypair(privkey2)).pubkey;
+          expect(pubkey2).toEqual(fromHex("3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c"));
+
+          const privkey3 = fromHex("c5aa8df43f9f837bedb7442f31dcb7b166d38535076f094b85ce3a2e0b4458f7");
+          const pubkey3 = (await Ed25519.makeKeypair(privkey3)).pubkey;
+          expect(pubkey3).toEqual(fromHex("fc51cd8e6218a1a38da47ed00230f0580816ed13ba3303ac5deb911548908025"));
+
+          const privkey4 = fromHex("f5e5767cf153319517630f226876b86c8160cc583bc013744c6bf255f5cc0ee5");
+          const pubkey4 = (await Ed25519.makeKeypair(privkey4)).pubkey;
+          expect(pubkey4).toEqual(fromHex("278117fc144c72340f67d0f2316e8386ceffbf2b2428c9c51fef7c597f1d426e"));
+        }
+
+        {
           // seed too short
           const seed = fromHex("43a9c17ccbb0e767ea29ce1f10813afde5f1e0a7a504e89b4d2cc2b952b8e0");
           await Ed25519.makeKeypair(seed)
