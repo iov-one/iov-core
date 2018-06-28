f8c23cd96b0f2aea8c426c5b5c47ab38d82fb406
Simon Warta <simon@kullo.net>
2018-06-28 16:51:19 +0200

    Update auto-generated type files

--- packages/iov-crypto/types/slip0010.d.ts
+++ packages/iov-crypto/types/slip0010.d.ts
@@ -5,7 +5,7 @@ export interface Slip0010Result {
 }
 export declare enum Slip0010Curve {
     Secp256k1 = "Bitcoin seed",
-    Ed25519 = "ed25519 seed",
+    Ed25519 = "ed25519 seed"
 }
 export declare class Slip0010RawIndex extends Uint32 {
     static hardened(hardenedIndex: number): Slip0010RawIndex;
@@ -14,10 +14,10 @@ export declare class Slip0010RawIndex extends Uint32 {
 }
 export declare class Slip0010 {
     static derivePath(curve: Slip0010Curve, seed: Uint8Array, path: ReadonlyArray<Slip0010RawIndex>): Slip0010Result;
-    private static master(curve, seed);
-    private static child(curve, parentPrivkey, parentChainCode, rawIndex);
-    private static childImpl(curve, parentPrivkey, parentChainCode, rawIndex, i);
-    private static isZero(privkey);
-    private static isGteN(curve, privkey);
-    private static n(curve);
+    private static master;
+    private static child;
+    private static childImpl;
+    private static isZero;
+    private static isGteN;
+    private static n;
 }
