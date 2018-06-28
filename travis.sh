8e3275e71cd3f7b04540107967f964291550626b
Simon Warta <simon@kullo.net>
2018-06-28 18:10:47 +0200

    Run bns-codec browser tests on Travis

--- travis.sh
+++ travis.sh
@@ -16,16 +16,18 @@ fi
 
 yarn test
 
-(
-  # Test browser
-  cd packages/iov-crypto
+# Test browser
+for PACKAGE in bns-codec iov-crypto; do
+  (
+    cd "packages/$PACKAGE"
 
-  yarn test-chrome
+    yarn test-chrome
 
-  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    yarn test-safari
+    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+      yarn test-safari
 
-    # Firefox does not run on Linux VMs because "no DISPLAY environment variable specified"
-    yarn test-firefox
-  fi
-)
+      # Firefox does not run on Linux VMs because "no DISPLAY environment variable specified"
+      yarn test-firefox
+    fi
+  )
+done
