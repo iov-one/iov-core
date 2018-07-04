d384b46b6899df10304205e8ece75ec2bb8ff7b0
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-04 18:55:40 +0200

    Verify signature dumped from cli script

---
+++ packages/ledger-bns/src/verify.spec.ts
@@ -0,0 +1,21 @@
+import { Ed25519, Encoding, Sha512 } from "@iov/crypto";
+
+describe("Verifies sample signature from ledger", () => {
+  const message = Encoding.fromHex("0a440a148a59e1dac0b8c5b59a9b2cfabe0be3fa6c2279f8121422feea060e235b06ffeef19015a50cdcb97d4f011a0808fa011a03455448220c466c61742032353020455448");
+  const messageHash = Encoding.fromHex("a26329dc1fa6cea63f7353c59693d0dd7b91d5de90afad9e62d884db943c6f7929830997ba1c5c8919da16d3464445ebe519fd6821973d2143e6fcb879e18f44");
+  const pubkey = Encoding.fromHex("4cb2a15864611e8af56637047eb7e81d780774fd7bb259d9409ab9662879954b");
+  const signature = Encoding.fromHex("4d9baafcd9e0aaec6cb19a91e859eef5209a75e8e45c396c012ab41cae41fbfa447288cd0ee6a4aa66cfc0fc630f07f50bf32dca0e0e669b4b242724791d110f");
+
+  it("Produces proper sha512 hash", () => {
+    const hash = new Sha512(message).digest();
+    expect(messageHash).toEqual(hash);
+  });
+
+  it("Produces valid signature", async done => {
+    const unhashed = await Ed25519.verifySignature(signature, message, pubkey);
+    expect(unhashed).toEqual(false);
+    const ok = await Ed25519.verifySignature(signature, messageHash, pubkey);
+    expect(ok).toEqual(true);
+    done();
+  });
+});
