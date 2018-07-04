5c1ab003ca884e6b5718797b0b6b15fea0933d0a
Simon Warta <simon@kullo.net>
2018-07-04 16:31:34 +0200

    Remove obsolete UserProfile.createIn

--- packages/iov-keycontrol/src/userprofile.spec.ts
+++ packages/iov-keycontrol/src/userprofile.spec.ts
@@ -13,21 +13,6 @@ describe("UserProfile", () => {
     expect(profile).toBeTruthy();
   });
 
-  it("can be created in an empty store", done => {
-    (async () => {
-      const storage: MemDown<string, string> = MemDownConstructor<string, string>();
-      await UserProfile.createIn(storage);
-      const profile = await UserProfile.loadFrom(storage);
-      expect(profile).toBeTruthy();
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
-  });
-
   it("can be stored", done => {
     (async () => {
       const storage: MemDown<string, string> = MemDownConstructor<string, string>();
--- packages/iov-keycontrol/src/userprofile.ts
+++ packages/iov-keycontrol/src/userprofile.ts
@@ -8,17 +8,6 @@ const storageKeyCreatedAt = "created_at";
 const storageKeyKeyring = "keyring";
 
 export class UserProfile {
-  // this will clear everything in storage and create a new UserProfile
-  public static async createIn(storage: AbstractLevelDOWN<string, string>): Promise<void> {
-    const creationDb = levelup(storage);
-    await UserProfile.clearDb(creationDb);
-
-    await creationDb.put(storageKeyCreatedAt, "1985-04-12T23:20:50.52Z");
-    await creationDb.put(storageKeyKeyring, new Keyring().serialize());
-
-    await creationDb.close();
-  }
-
   public static async loadFrom(storage: AbstractLevelDOWN<string, string>): Promise<UserProfile> {
     const db = levelup(storage);
     const createdAt = new ReadonlyDate(await db.get(storageKeyCreatedAt)); // TODO: add strict RFC 3339 parser
