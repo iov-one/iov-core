f89217095f2587945d161ca268c3c6d7fbc37e49
Simon Warta <simon@kullo.net>
2018-07-05 18:08:52 +0200

    Remove unused MemoryStreamUtils

--- packages/iov-keycontrol/src/utils.ts
+++ packages/iov-keycontrol/src/utils.ts
@@ -1,6 +1,5 @@
 import { AbstractLevelDOWN } from "abstract-leveldown";
 import { LevelUp } from "levelup";
-import { MemoryStream } from "xstream";
 
 export class DatabaseUtils {
   public static clear<K, V>(db: LevelUp<AbstractLevelDOWN<K, V>>): Promise<void> {
@@ -20,29 +19,6 @@ export class DatabaseUtils {
           }
           resolve();
         });
-    });
-  }
-}
-
-export class MemoryStreamUtils {
-  public static value<T>(stream: MemoryStream<T>): Promise<T> {
-    return new Promise((resolve, reject) => {
-      const listener = {
-        next: (value: T) => {
-          stream.removeListener(listener);
-          resolve(value);
-        },
-        error: (error: any) => {
-          stream.removeListener(listener);
-          reject(error);
-        },
-        complete: () => {
-          stream.removeListener(listener);
-          reject("Stream completed without providing a value");
-        },
-      };
-
-      stream.addListener(listener);
     });
   }
 }
