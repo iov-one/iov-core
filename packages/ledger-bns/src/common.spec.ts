df9783b4cade37b392682c00f7ef63f2c0e49154
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-05 19:04:29 +0200

    Skip ledger tests based on TRAVIS env variable

--- packages/ledger-bns/src/common.spec.ts
+++ packages/ledger-bns/src/common.spec.ts
@@ -1,7 +1,5 @@
-import { isatty } from "tty";
-
 // this should return true to skip ledger tests (to run on CI)
 //
-// current solution: skip if not attached to a tty (non-interactive)
-// should look for a better solution, especially if we test in browser
-export const skipTests = (): boolean => !isatty(0);
+// current solution: check for TRAVIS=true environmental variable
+// Does this work in browsers??
+export const skipTests = (): boolean => !!process.env.TRAVIS;
