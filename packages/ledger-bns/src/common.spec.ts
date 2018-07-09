8a698e560437e1ae9f6108a388b83d3b895d4b7f
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-09 17:11:08 +0200

    Verify hardware derivation matches software slip0010

--- packages/ledger-bns/src/common.spec.ts
+++ packages/ledger-bns/src/common.spec.ts
@@ -3,21 +3,21 @@
 // current solution: check for LEDGER_ENABLED=true environmental variable
 // Does this work in browsers??
 
-export const skipTests = (): boolean => !process.env.LEDGER_ENABLED;
+export const skipTests = (): boolean => !process.env.LEDGER_ENABLED && !process.env.LEDGER_ALL;
 export const pendingWithoutLedger = (): void => {
   if (skipTests()) {
     pending("Set LEDGER_ENABLED to run ledger tests");
   }
 };
 
-export const skipSeededTests = (): boolean => !process.env.LEDGER_SEEDED;
+export const skipSeededTests = (): boolean => !process.env.LEDGER_SEEDED && !process.env.LEDGER_ALL;
 export const pendingWithoutSeededLedger = (): void => {
   if (skipSeededTests()) {
-    pending("Set LEDGER_SEEDED to enable tests, ledger must be seeded with test key");
+    pending("Set LEDGER_SEEDED to enable derivation tests, ledger must be seeded with test key");
   }
 };
 
-export const skipInteractiveTests = (): boolean => !process.env.LEDGER_INTERACTIVE;
+export const skipInteractiveTests = (): boolean => !process.env.LEDGER_INTERACTIVE && !process.env.LEDGER_ALL;
 export const pendingWithoutInteractiveLedger = (): void => {
   if (skipInteractiveTests()) {
     pending("Set LEDGER_INTERACTIVE to run ledger tests");
---
+++ packages/ledger-bns/src/derivation.spec.ts
@@ -0,0 +1,58 @@
+import { Bip39, Ed25519, EnglishMnemonic, Slip0010, Slip0010Curve, Slip0010RawIndex } from "@iov/crypto";
+
+import { getPublicKeyWithPath } from "./app";
+import { pendingWithoutSeededLedger, skipSeededTests } from "./common.spec";
+import { connectToFirstLedger } from "./exchange";
+
+describe("Check key derivation", () => {
+  // tslint:disable-next-line:no-let
+  let transport: Transport | undefined;
+
+  // try 12 word seed phrase to enter in ledger for these tests
+  // you have to reinit the ledger for this to work
+  const phrase = "tell fresh liquid vital machine rhythm uncle tomato grow room vacuum neutral";
+
+  const mneumonic = new EnglishMnemonic(phrase);
+  const prefix = Slip0010RawIndex.hardened(4804438); // from ed25519simpleaddress
+
+  beforeAll(() => {
+    if (!skipSeededTests()) {
+      transport = connectToFirstLedger();
+    }
+  });
+
+  it("compare public keys", done => {
+    pendingWithoutSeededLedger();
+
+    const checkKey = async (path: number) => {
+      const hdPath: ReadonlyArray<Slip0010RawIndex> = [prefix, Slip0010RawIndex.hardened(path)];
+      const seed = await Bip39.mnemonicToSeed(mneumonic);
+      const res = Slip0010.derivePath(Slip0010Curve.Ed25519, seed, hdPath);
+
+      const keypair = await Ed25519.makeKeypair(res.privkey);
+      const swPubkey = keypair.pubkey;
+      expect(swPubkey).toBeTruthy();
+      expect(swPubkey.length).toBe(32);
+
+      const hwPubkey = await getPublicKeyWithPath(transport, path);
+      expect(hwPubkey).toBeTruthy();
+      expect(hwPubkey.length).toBe(32);
+
+      expect(new Uint8Array(hwPubkey)).toEqual(swPubkey);
+    };
+
+    const checkMultipleKeys = async () => {
+      await checkKey(3);
+      await checkKey(0);
+      await checkKey(17);
+      await checkKey(1346);
+      await checkKey(123456);
+      await checkKey(53252985);
+    };
+
+    // run a few different
+    checkMultipleKeys()
+      .catch(err => fail(err))
+      .then(done);
+  });
+});
