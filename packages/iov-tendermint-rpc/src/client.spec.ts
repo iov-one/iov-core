d605251cf4d59e326b4dba8c28e7010b0dc88427
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-12 17:24:54 +0200

    Add extra check for querying block

--- packages/iov-tendermint-rpc/src/client.spec.ts
+++ packages/iov-tendermint-rpc/src/client.spec.ts
@@ -86,6 +86,7 @@ describe("Verify client calls on tendermint w/ kvstore app", () => {
     pendingWithoutTendermint();
     const client = new Client(new HttpClient(tendermintUrl), v0_20);
 
+<<<<<<< 730e5a9b45113c3228aa50ddc21e4b273425ef30
     const find = randomId();
     const me = randomId();
     const tx = buildKvTx(find, me);
@@ -115,5 +116,46 @@ describe("Verify client calls on tendermint w/ kvstore app", () => {
     // should return same info as querying directly,
     // except without the proof
     expect(s.txs[0]).toEqual({ ...r, proof: undefined });
+=======
+    const verifyTxResponses = async () => {
+      const find = randomId();
+      const me = randomId();
+      const tx = buildKvTx(find, me);
+
+      const txRes = await client.broadcastTxCommit({ tx });
+      expect(responses.txCommitSuccess(txRes)).toBeTruthy();
+      expect(txRes.height).toBeTruthy();
+      const height: number = txRes.height || 0; // || 0 for type system
+      expect(txRes.hash.length).not.toEqual(0);
+      const hash = txRes.hash;
+
+      // find by hash - does it match?
+      const r = await client.tx({ hash, prove: true });
+      // both values come from rpc, so same type (Buffer/Uint8Array)
+      expect(r.hash).toEqual(hash);
+      // force the type when comparing to locally generated value
+      expect(new Uint8Array(r.tx)).toEqual(tx);
+      expect(r.height).toEqual(height);
+      expect(r.proof).toBeTruthy();
+
+      // txSearch - you must enable the indexer when running
+      // tendermint, else you get empty results
+      const query = `app.key='${find}'` as QueryString;
+      const s = await client.txSearch({ query, page: 1, per_page: 30 });
+      // should find the tx
+      expect(s.totalCount).toEqual(1);
+      // should return same info as querying directly,
+      // except without the proof
+      expect(s.txs[0]).toEqual({ ...r, proof: undefined });
+
+      // and let's query the block itself to see this transaction
+      const block = await client.block(height);
+      expect(block.blockMeta.header.numTxs).toEqual(1);
+      expect(block.block.txs.length).toEqual(1);
+      expect(block.block.txs[0]).toEqual(tx);
+    };
+
+    return verifyTxResponses().catch(fail);
+>>>>>>> Add extra check for querying block
   });
 });
