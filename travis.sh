d30e8f68288168be3fffc706a2f10e22941b8ec8
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-07-12 10:48:25 +0200

    Rename tendermint-rpc to iov-tendermint-rpc

--- travis.sh
+++ travis.sh
@@ -17,7 +17,7 @@ fi
 yarn test
 
 # Test browser
-for PACKAGE in bns-codec iov-crypto iov-keycontrol tendermint-rpc; do
+for PACKAGE in bns-codec iov-crypto iov-keycontrol iov-tendermint-rpc; do
   (
     cd "packages/$PACKAGE"
 
