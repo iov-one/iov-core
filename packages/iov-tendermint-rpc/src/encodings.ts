c926dc76db35b9c884277b9fb88265fe74202e09
Simon Warta <simon@kullo.net>
2018-07-27 17:14:30 +0200

    Remove unused DateTime.encode

--- packages/iov-tendermint-rpc/src/encodings.ts
+++ packages/iov-tendermint-rpc/src/encodings.ts
@@ -60,10 +60,6 @@ export class Base64 {
 }
 
 export class DateTime {
-  public static encode(date: ReadonlyDate): DateTimeString {
-    return date.toISOString() as DateTimeString;
-  }
-
   public static decode(dateTimeString: DateTimeString): ReadonlyDate {
     return new ReadonlyDate(dateTimeString);
   }
