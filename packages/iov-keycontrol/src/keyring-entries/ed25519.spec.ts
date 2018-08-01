47cadecc42abc70fd67507a1d800fdb290d75a4b
Simon Warta <simon@kullo.net>
2018-08-01 12:32:59 +0200

    Simplify async boilerplate in Ed25519KeyringEntry tests

--- packages/iov-keycontrol/src/keyring-entries/ed25519.spec.ts
+++ packages/iov-keycontrol/src/keyring-entries/ed25519.spec.ts
@@ -29,95 +29,63 @@ describe("Ed25519KeyringEntry", () => {
     expect(entry.label.value).toBeUndefined();
   });
 
-  it("can create an identity", done => {
-    (async () => {
-      const keyringEntry = new Ed25519KeyringEntry();
-      const newIdentity = await keyringEntry.createIdentity();
-      expect(keyringEntry.getIdentities().length).toEqual(1);
+  it("can create an identity", async () => {
+    const keyringEntry = new Ed25519KeyringEntry();
+    const newIdentity = await keyringEntry.createIdentity();
+    expect(keyringEntry.getIdentities().length).toEqual(1);
 
-      const firstIdentity = keyringEntry.getIdentities()[0];
-      expect(newIdentity.pubkey.algo).toEqual(firstIdentity.pubkey.algo);
-      expect(newIdentity.pubkey.data).toEqual(firstIdentity.pubkey.data);
-      expect(newIdentity.label).toEqual(firstIdentity.label);
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
+    const firstIdentity = keyringEntry.getIdentities()[0];
+    expect(newIdentity.pubkey.algo).toEqual(firstIdentity.pubkey.algo);
+    expect(newIdentity.pubkey.data).toEqual(firstIdentity.pubkey.data);
+    expect(newIdentity.label).toEqual(firstIdentity.label);
   });
 
-  it("can create multiple identities", done => {
-    (async () => {
-      const keyringEntry = new Ed25519KeyringEntry();
-      const newIdentity1 = await keyringEntry.createIdentity(); // 1
-      await keyringEntry.createIdentity(); // 2
-      await keyringEntry.createIdentity(); // 3
-      await keyringEntry.createIdentity(); // 4
-      const newIdentity5 = await keyringEntry.createIdentity(); // 5
-      expect(keyringEntry.getIdentities().length).toEqual(5);
+  it("can create multiple identities", async () => {
+    const keyringEntry = new Ed25519KeyringEntry();
+    const newIdentity1 = await keyringEntry.createIdentity(); // 1
+    await keyringEntry.createIdentity(); // 2
+    await keyringEntry.createIdentity(); // 3
+    await keyringEntry.createIdentity(); // 4
+    const newIdentity5 = await keyringEntry.createIdentity(); // 5
+    expect(keyringEntry.getIdentities().length).toEqual(5);
 
-      const firstIdentity = keyringEntry.getIdentities()[0];
-      expect(newIdentity1.pubkey.algo).toEqual(firstIdentity.pubkey.algo);
-      expect(newIdentity1.pubkey.data).toEqual(firstIdentity.pubkey.data);
-      expect(newIdentity1.label).toEqual(firstIdentity.label);
+    const firstIdentity = keyringEntry.getIdentities()[0];
+    expect(newIdentity1.pubkey.algo).toEqual(firstIdentity.pubkey.algo);
+    expect(newIdentity1.pubkey.data).toEqual(firstIdentity.pubkey.data);
+    expect(newIdentity1.label).toEqual(firstIdentity.label);
 
-      const lastIdentity = keyringEntry.getIdentities()[4];
-      expect(newIdentity5.pubkey.algo).toEqual(lastIdentity.pubkey.algo);
-      expect(newIdentity5.pubkey.data).toEqual(lastIdentity.pubkey.data);
-      expect(newIdentity5.label).toEqual(lastIdentity.label);
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
+    const lastIdentity = keyringEntry.getIdentities()[4];
+    expect(newIdentity5.pubkey.algo).toEqual(lastIdentity.pubkey.algo);
+    expect(newIdentity5.pubkey.data).toEqual(lastIdentity.pubkey.data);
+    expect(newIdentity5.label).toEqual(lastIdentity.label);
   });
 
-  it("can set, change and unset an identity label", done => {
-    (async () => {
-      const keyringEntry = new Ed25519KeyringEntry();
-      const newIdentity = await keyringEntry.createIdentity();
-      expect(keyringEntry.getIdentities()[0].label).toBeUndefined();
+  it("can set, change and unset an identity label", async () => {
+    const keyringEntry = new Ed25519KeyringEntry();
+    const newIdentity = await keyringEntry.createIdentity();
+    expect(keyringEntry.getIdentities()[0].label).toBeUndefined();
 
-      keyringEntry.setIdentityLabel(newIdentity, "foo");
-      expect(keyringEntry.getIdentities()[0].label).toEqual("foo");
+    keyringEntry.setIdentityLabel(newIdentity, "foo");
+    expect(keyringEntry.getIdentities()[0].label).toEqual("foo");
 
-      keyringEntry.setIdentityLabel(newIdentity, "bar");
-      expect(keyringEntry.getIdentities()[0].label).toEqual("bar");
+    keyringEntry.setIdentityLabel(newIdentity, "bar");
+    expect(keyringEntry.getIdentities()[0].label).toEqual("bar");
 
-      keyringEntry.setIdentityLabel(newIdentity, undefined);
-      expect(keyringEntry.getIdentities()[0].label).toBeUndefined();
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
+    keyringEntry.setIdentityLabel(newIdentity, undefined);
+    expect(keyringEntry.getIdentities()[0].label).toBeUndefined();
   });
 
-  it("can sign", done => {
-    (async () => {
-      const keyringEntry = new Ed25519KeyringEntry();
-      const newIdentity = await keyringEntry.createIdentity();
+  it("can sign", async () => {
+    const keyringEntry = new Ed25519KeyringEntry();
+    const newIdentity = await keyringEntry.createIdentity();
 
-      expect(keyringEntry.canSign.value).toEqual(true);
+    expect(keyringEntry.canSign.value).toEqual(true);
 
-      const tx = new Uint8Array([0x11, 0x22, 0x33]) as SignableBytes;
-      const chainId = "some-chain" as ChainId;
-      const signature = await keyringEntry.createTransactionSignature(newIdentity, tx, PrehashType.None, chainId);
-      expect(signature).toBeTruthy();
-      expect(signature.length).toEqual(64);
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
+    const tx = new Uint8Array([0x11, 0x22, 0x33]) as SignableBytes;
+    const chainId = "some-chain" as ChainId;
+    const signature = await keyringEntry.createTransactionSignature(newIdentity, tx, PrehashType.None, chainId);
+    expect(signature).toBeTruthy();
+    expect(signature.length).toEqual(64);
   });
 
   it("can sign with different prehash types", async () => {
@@ -155,55 +123,47 @@ describe("Ed25519KeyringEntry", () => {
     expect(await entry.createTransactionSignature(mainIdentity, bytes, PrehashType.Sha512, chainId)).toEqual(expectedSha512);
   });
 
-  it("can serialize multiple identities", done => {
-    (async () => {
-      const keyringEntry = new Ed25519KeyringEntry();
-      keyringEntry.setLabel("entry with 3 identities");
-      const identity1 = await keyringEntry.createIdentity();
-      const identity2 = await keyringEntry.createIdentity();
-      const identity3 = await keyringEntry.createIdentity();
-      keyringEntry.setIdentityLabel(identity1, undefined);
-      keyringEntry.setIdentityLabel(identity2, "");
-      keyringEntry.setIdentityLabel(identity3, "foo");
+  it("can serialize multiple identities", async () => {
+    const keyringEntry = new Ed25519KeyringEntry();
+    keyringEntry.setLabel("entry with 3 identities");
+    const identity1 = await keyringEntry.createIdentity();
+    const identity2 = await keyringEntry.createIdentity();
+    const identity3 = await keyringEntry.createIdentity();
+    keyringEntry.setIdentityLabel(identity1, undefined);
+    keyringEntry.setIdentityLabel(identity2, "");
+    keyringEntry.setIdentityLabel(identity3, "foo");
 
-      const serialized = keyringEntry.serialize();
-      expect(serialized).toBeTruthy();
-      expect(serialized.length).toBeGreaterThan(100);
+    const serialized = keyringEntry.serialize();
+    expect(serialized).toBeTruthy();
+    expect(serialized.length).toBeGreaterThan(100);
 
-      const decoded = JSON.parse(serialized);
-      expect(decoded.label).toEqual("entry with 3 identities");
-      expect(decoded.identities).toBeTruthy();
-      expect(decoded.identities.length).toEqual(3);
-      expect(decoded.identities[0].localIdentity).toBeTruthy();
-      expect(decoded.identities[0].localIdentity.pubkey.algo).toEqual("ed25519");
-      expect(decoded.identities[0].localIdentity.pubkey.data).toMatch(/[0-9a-f]{64}/);
-      expect(decoded.identities[0].localIdentity.label).toBeUndefined();
-      expect(decoded.identities[0].privkey).toMatch(/[0-9a-f]{64}/);
-      expect(decoded.identities[1].localIdentity).toBeTruthy();
-      expect(decoded.identities[1].localIdentity.pubkey.algo).toEqual("ed25519");
-      expect(decoded.identities[1].localIdentity.pubkey.data).toMatch(/[0-9a-f]{64}/);
-      expect(decoded.identities[1].localIdentity.label).toEqual("");
-      expect(decoded.identities[1].privkey).toMatch(/[0-9a-f]{64}/);
-      expect(decoded.identities[2].localIdentity).toBeTruthy();
-      expect(decoded.identities[2].localIdentity.pubkey.algo).toEqual("ed25519");
-      expect(decoded.identities[2].localIdentity.pubkey.data).toMatch(/[0-9a-f]{64}/);
-      expect(decoded.identities[2].localIdentity.label).toEqual("foo");
-      expect(decoded.identities[2].privkey).toMatch(/[0-9a-f]{64}/);
+    const decoded = JSON.parse(serialized);
+    expect(decoded.label).toEqual("entry with 3 identities");
+    expect(decoded.identities).toBeTruthy();
+    expect(decoded.identities.length).toEqual(3);
+    expect(decoded.identities[0].localIdentity).toBeTruthy();
+    expect(decoded.identities[0].localIdentity.pubkey.algo).toEqual("ed25519");
+    expect(decoded.identities[0].localIdentity.pubkey.data).toMatch(/[0-9a-f]{64}/);
+    expect(decoded.identities[0].localIdentity.label).toBeUndefined();
+    expect(decoded.identities[0].privkey).toMatch(/[0-9a-f]{64}/);
+    expect(decoded.identities[1].localIdentity).toBeTruthy();
+    expect(decoded.identities[1].localIdentity.pubkey.algo).toEqual("ed25519");
+    expect(decoded.identities[1].localIdentity.pubkey.data).toMatch(/[0-9a-f]{64}/);
+    expect(decoded.identities[1].localIdentity.label).toEqual("");
+    expect(decoded.identities[1].privkey).toMatch(/[0-9a-f]{64}/);
+    expect(decoded.identities[2].localIdentity).toBeTruthy();
+    expect(decoded.identities[2].localIdentity.pubkey.algo).toEqual("ed25519");
+    expect(decoded.identities[2].localIdentity.pubkey.data).toMatch(/[0-9a-f]{64}/);
+    expect(decoded.identities[2].localIdentity.label).toEqual("foo");
+    expect(decoded.identities[2].privkey).toMatch(/[0-9a-f]{64}/);
 
-      // keys are different
-      expect(decoded.identities[0].localIdentity.pubkey.data).not.toEqual(decoded.identities[1].localIdentity.pubkey.data);
-      expect(decoded.identities[1].localIdentity.pubkey.data).not.toEqual(decoded.identities[2].localIdentity.pubkey.data);
-      expect(decoded.identities[2].localIdentity.pubkey.data).not.toEqual(decoded.identities[0].localIdentity.pubkey.data);
-      expect(decoded.identities[0].privkey).not.toEqual(decoded.identities[1].privkey);
-      expect(decoded.identities[1].privkey).not.toEqual(decoded.identities[2].privkey);
-      expect(decoded.identities[2].privkey).not.toEqual(decoded.identities[0].privkey);
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
+    // keys are different
+    expect(decoded.identities[0].localIdentity.pubkey.data).not.toEqual(decoded.identities[1].localIdentity.pubkey.data);
+    expect(decoded.identities[1].localIdentity.pubkey.data).not.toEqual(decoded.identities[2].localIdentity.pubkey.data);
+    expect(decoded.identities[2].localIdentity.pubkey.data).not.toEqual(decoded.identities[0].localIdentity.pubkey.data);
+    expect(decoded.identities[0].privkey).not.toEqual(decoded.identities[1].privkey);
+    expect(decoded.identities[1].privkey).not.toEqual(decoded.identities[2].privkey);
+    expect(decoded.identities[2].privkey).not.toEqual(decoded.identities[0].privkey);
   });
 
   it("can deserialize", () => {
@@ -240,34 +200,26 @@ describe("Ed25519KeyringEntry", () => {
     }
   });
 
-  it("can serialize and restore a full keyring entry", done => {
-    (async () => {
-      const original = new Ed25519KeyringEntry();
-      const identity1 = await original.createIdentity();
-      const identity2 = await original.createIdentity();
-      const identity3 = await original.createIdentity();
-      original.setIdentityLabel(identity1, undefined);
-      original.setIdentityLabel(identity2, "");
-      original.setIdentityLabel(identity3, "foo");
+  it("can serialize and restore a full keyring entry", async () => {
+    const original = new Ed25519KeyringEntry();
+    const identity1 = await original.createIdentity();
+    const identity2 = await original.createIdentity();
+    const identity3 = await original.createIdentity();
+    original.setIdentityLabel(identity1, undefined);
+    original.setIdentityLabel(identity2, "");
+    original.setIdentityLabel(identity3, "foo");
 
-      const restored = new Ed25519KeyringEntry(original.serialize());
+    const restored = new Ed25519KeyringEntry(original.serialize());
 
-      // pubkeys and labels match
-      expect(original.getIdentities()).toEqual(restored.getIdentities());
+    // pubkeys and labels match
+    expect(original.getIdentities()).toEqual(restored.getIdentities());
 
-      // privkeys match
-      const tx = new Uint8Array([]) as SignableBytes;
-      const chainId = "" as ChainId;
-      expect(await original.createTransactionSignature(identity1, tx, PrehashType.None, chainId)).toEqual(await restored.createTransactionSignature(identity1, tx, PrehashType.None, chainId));
-      expect(await original.createTransactionSignature(identity2, tx, PrehashType.None, chainId)).toEqual(await restored.createTransactionSignature(identity2, tx, PrehashType.None, chainId));
-      expect(await original.createTransactionSignature(identity3, tx, PrehashType.None, chainId)).toEqual(await restored.createTransactionSignature(identity3, tx, PrehashType.None, chainId));
-
-      done();
-    })().catch(error => {
-      setTimeout(() => {
-        throw error;
-      });
-    });
+    // privkeys match
+    const tx = new Uint8Array([]) as SignableBytes;
+    const chainId = "" as ChainId;
+    expect(await original.createTransactionSignature(identity1, tx, PrehashType.None, chainId)).toEqual(await restored.createTransactionSignature(identity1, tx, PrehashType.None, chainId));
+    expect(await original.createTransactionSignature(identity2, tx, PrehashType.None, chainId)).toEqual(await restored.createTransactionSignature(identity2, tx, PrehashType.None, chainId));
+    expect(await original.createTransactionSignature(identity3, tx, PrehashType.None, chainId)).toEqual(await restored.createTransactionSignature(identity3, tx, PrehashType.None, chainId));
   });
 
   it("can be cloned", () => {
