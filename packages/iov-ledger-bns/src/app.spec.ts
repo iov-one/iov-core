eb5204c047f4c4bddc979a7476f689ae4b99b466
Simon Warta <simon@kullo.net>
2018-08-02 13:28:17 +0200

    Cleanup some ledger BNS tests

--- packages/iov-ledger-bns/src/app.spec.ts
+++ packages/iov-ledger-bns/src/app.spec.ts
@@ -76,107 +76,96 @@ describe("Sign with ledger app", () => {
     }
   });
 
-  it("can properly sign valid message", done => {
+  it("can properly sign valid message", async () => {
     pendingWithoutInteractiveLedger();
 
     // this is pre-generated signbytes
     const message = Encoding.fromHex("00cafe0008746573742d31323300000000000000110a440a1403694b56200b605a3a726304b6dfaa6e916458ee12146bc29ffe4fc6a4b2395c3f47b5ca9dfa377295f91a0808fa011a03455448220c54657374207061796d656e74");
-    const messageHash = new Sha512(message).digest();
 
-    const validateSig = async () => {
-      const pubkey = await getPublicKey(transport);
-      expect(pubkey.length).toEqual(32);
-      const signature = await signTransaction(transport, message);
-      expect(signature.length).toEqual(64);
-      const ok = await Ed25519.verifySignature(signature, messageHash, pubkey);
-      expect(ok).toEqual(true);
-    };
+    const pubkey = await getPublicKey(transport);
+    expect(pubkey.length).toEqual(32);
 
-    validateSig()
-      .catch(err => fail(err))
-      .then(done);
+    const signature = await signTransaction(transport, message);
+    expect(signature.length).toEqual(64);
+
+    const prehash = new Sha512(message).digest();
+    const ok = await Ed25519.verifySignature(signature, prehash, pubkey);
+    expect(ok).toEqual(true);
   });
 
   // Note: verify that this display expected info (1234.789 LGR and
   // 0123... recipient) when verifying signature
-  it("is compatible with our codecs", done => {
+  it("is compatible with our codecs", async () => {
     pendingWithoutInteractiveLedger();
 
-    const validateSig = async () => {
-      const pubkey = await getPublicKey(transport);
-      expect(pubkey.length).toEqual(32);
+    const pubkey = await getPublicKey(transport);
+    expect(pubkey.length).toEqual(32);
 
-      const sender: PublicKeyBundle = {
-        algo: Algorithm.ED25519,
-        data: pubkey as PublicKeyBytes,
-      };
-
-      const tx: SendTx = {
-        kind: TransactionKind.SEND,
-        chainId: "test-bns-ledger" as ChainId,
-        recipient: Encoding.fromHex("0123456789012345678901234567890123456789") as RecipientId,
-        amount: {
-          // 1234.789 LGR
-          whole: 1234,
-          fractional: 789000000,
-          tokenTicker: "LGR" as TokenTicker,
-        },
-        signer: sender,
-        memo: "Hi Mom!",
-      };
-      const nonce = Long.fromNumber(123) as Nonce;
-      const { bytes: message } = bnsCodec.bytesToSign(tx, nonce);
-      const messageHash = new Sha512(message).digest();
-
-      const signature = await signTransaction(transport, message);
-      expect(signature.length).toEqual(64);
-      const ok = await Ed25519.verifySignature(signature, messageHash, pubkey);
-      expect(ok).toEqual(true);
+    const sender: PublicKeyBundle = {
+      algo: Algorithm.ED25519,
+      data: pubkey as PublicKeyBytes,
     };
 
-    validateSig()
-      .catch(err => fail(err))
-      .then(done);
+    const tx: SendTx = {
+      kind: TransactionKind.SEND,
+      chainId: "test-bns-ledger" as ChainId,
+      recipient: Encoding.fromHex("0123456789012345678901234567890123456789") as RecipientId,
+      amount: {
+        // 1234.789 LGR
+        whole: 1234,
+        fractional: 789000000,
+        tokenTicker: "LGR" as TokenTicker,
+      },
+      signer: sender,
+      memo: "Hi Mom!",
+    };
+    const nonce = Long.fromNumber(123) as Nonce;
+    const message = bnsCodec.bytesToSign(tx, nonce).bytes;
+
+    const signature = await signTransaction(transport, message);
+    expect(signature.length).toEqual(64);
+
+    // verify signature from Ledger
+    const prehash = new Sha512(message).digest();
+    const ok = await Ed25519.verifySignature(signature, prehash, pubkey);
+    expect(ok).toEqual(true);
   });
 
   // this is as above, but verifying a different path also works
-  it("can sign with multiple paths", done => {
+  it("can sign with index", async () => {
     pendingWithoutInteractiveLedger();
-    const path = 0x787;
 
-    const validateSig = async () => {
-      const pubkey = await getPublicKeyWithIndex(transport, path);
-      expect(pubkey.length).toEqual(32);
+    const simpleAddressIndex = 0x787;
 
-      const sender: PublicKeyBundle = {
-        algo: Algorithm.ED25519,
-        data: pubkey as PublicKeyBytes,
-      };
+    const pubkey = await getPublicKeyWithIndex(transport, simpleAddressIndex);
+    expect(pubkey.length).toEqual(32);
 
-      const tx: SendTx = {
-        kind: TransactionKind.SEND,
-        chainId: "test-ledger-paths" as ChainId,
-        recipient: Encoding.fromHex("1234ABCD0000AA0000FFFF0000AA00001234ABCD") as RecipientId,
-        amount: {
-          // 77.01001 PATH
-          whole: 77,
-          fractional: 10010000,
-          tokenTicker: "PATH" as TokenTicker,
-        },
-        signer: sender,
-      };
-      const nonce = Long.fromNumber(5) as Nonce;
-      const { bytes: message } = bnsCodec.bytesToSign(tx, nonce);
-      const messageHash = new Sha512(message).digest();
-
-      const signature = await signTransactionWithIndex(transport, message, path);
-      expect(signature.length).toEqual(64);
-      const ok = await Ed25519.verifySignature(signature, messageHash, pubkey);
-      expect(ok).toEqual(true);
+    const sender: PublicKeyBundle = {
+      algo: Algorithm.ED25519,
+      data: pubkey as PublicKeyBytes,
     };
 
-    validateSig()
-      .catch(err => fail(err))
-      .then(done);
+    const tx: SendTx = {
+      kind: TransactionKind.SEND,
+      chainId: "test-ledger-paths" as ChainId,
+      recipient: Encoding.fromHex("1234ABCD0000AA0000FFFF0000AA00001234ABCD") as RecipientId,
+      amount: {
+        // 77.01001 PATH
+        whole: 77,
+        fractional: 10010000,
+        tokenTicker: "PATH" as TokenTicker,
+      },
+      signer: sender,
+    };
+    const nonce = Long.fromNumber(5) as Nonce;
+    const message = bnsCodec.bytesToSign(tx, nonce).bytes;
+
+    const signature = await signTransactionWithIndex(transport, message, simpleAddressIndex);
+    expect(signature.length).toEqual(64);
+
+    // verify signature from Ledger
+    const prehash = new Sha512(message).digest();
+    const ok = await Ed25519.verifySignature(signature, prehash, pubkey);
+    expect(ok).toEqual(true);
   });
 });
