d18af7536d86533f60f5a9c2e162ba6ef6b7f8e5
Simon Warta <simon@kullo.net>
2018-08-02 14:28:39 +0200

    Add multi identity creation test

--- packages/iov-ledger-keyringentry/src/ledgerkeyringentry.spec.ts
+++ packages/iov-ledger-keyringentry/src/ledgerkeyringentry.spec.ts
@@ -1,6 +1,9 @@
+import { Encoding } from "@iov/encoding";
 import { Algorithm } from "@iov/tendermint-types";
 
 import { LedgerKeyringEntry } from "./ledgerkeyringentry";
+
+const { toHex } = Encoding;
 
 const skipTests = (): boolean => !process.env.LEDGER_ENABLED && !process.env.LEDGER_ALL;
 const pendingWithoutLedger = (): void => {
@@ -54,5 +57,32 @@ describe("LedgerKeyringEntry", () => {
     expect(newIdentity.pubkey.algo).toEqual(firstIdentity.pubkey.algo);
     expect(newIdentity.pubkey.data).toEqual(firstIdentity.pubkey.data);
     expect(newIdentity.label).toEqual(firstIdentity.label);
+  });
+
+  it("can create multiple identities", async () => {
+    pendingWithoutLedger();
+
+    const keyringEntry = new LedgerKeyringEntry();
+    const newIdentity1 = await keyringEntry.createIdentity();
+    const newIdentity2 = await keyringEntry.createIdentity();
+    const newIdentity3 = await keyringEntry.createIdentity();
+    const newIdentity4 = await keyringEntry.createIdentity();
+    const newIdentity5 = await keyringEntry.createIdentity();
+
+    // all pubkeys must be different
+    const pubkeySet = new Set([newIdentity1, newIdentity2, newIdentity3, newIdentity4, newIdentity5].map(i => toHex(i.pubkey.data)));
+    expect(pubkeySet.size).toEqual(5);
+
+    expect(keyringEntry.getIdentities().length).toEqual(5);
+
+    const firstIdentity = keyringEntry.getIdentities()[0];
+    expect(newIdentity1.pubkey.algo).toEqual(firstIdentity.pubkey.algo);
+    expect(newIdentity1.pubkey.data).toEqual(firstIdentity.pubkey.data);
+    expect(newIdentity1.label).toEqual(firstIdentity.label);
+
+    const lastIdentity = keyringEntry.getIdentities()[4];
+    expect(newIdentity5.pubkey.algo).toEqual(lastIdentity.pubkey.algo);
+    expect(newIdentity5.pubkey.data).toEqual(lastIdentity.pubkey.data);
+    expect(newIdentity5.label).toEqual(lastIdentity.label);
   });
 });
