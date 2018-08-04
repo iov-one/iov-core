b07ddbcdb150730ce38b0d1f69b0d989b6b3de52
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-08-05 00:46:49 +0200

    Render all dependencies for iov-web4 typedoc

--- packages/iov-web4/typedoc.js
+++ packages/iov-web4/typedoc.js
@@ -3,7 +3,13 @@ module.exports = {
   out: "docs",
   exclude: "**/*.spec.ts",
   target: "es6",
-  readme: "none",
+  readme: "api.md",
   mode: "file",
   excludePrivate: true,
+  excludeNotExported: true,
+  // this pulls in all dependencies
+  includeDeclarations: true,
+  // TODO: tweak this so we can ignore non iov (mono-repo) imports
+  excludeExternals: false,
+  externalPattern: "^((?!iov).)*$",
 }
