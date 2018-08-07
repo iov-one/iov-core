82ac2ced2a916e67b74a0665e2451d63d2caaec1
Simon Warta <simon@kullo.net>
2018-08-07 09:19:14 +0200

    Specify documentation titles
    This avoids @iov/core being called "type-tagger"

--- packages/iov-bcp-types/typedoc.js
+++ packages/iov-bcp-types/typedoc.js
@@ -1,7 +1,10 @@
+const packageJson = require("./package.json");
+
 module.exports = {
   out: "docs",
   src: ["./src"],
   target: "es6",
+  name: `${packageJson.name} Documentation`,
   readme: "none",
   mode: "file",
   includeDefinitions: true
--- packages/iov-encoding/typedoc.js
+++ packages/iov-encoding/typedoc.js
@@ -1,8 +1,11 @@
+const packageJson = require("./package.json");
+
 module.exports = {
   src: ["./src"],
   out: "docs",
   exclude: "**/*.spec.ts",
   target: "es6",
+  name: `${packageJson.name} Documentation`,
   readme: "none",
   mode: "file",
   excludePrivate: true,
