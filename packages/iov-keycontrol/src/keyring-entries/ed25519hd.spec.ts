80eec634943d297dfeecc10e5b0a87d76c736947
Simon Warta <simon@kullo.net>
2018-08-17 16:54:04 +0200

    make tslint happy

--- packages/iov-keycontrol/src/keyring-entries/ed25519hd.spec.ts
+++ packages/iov-keycontrol/src/keyring-entries/ed25519hd.spec.ts
@@ -8,6 +8,7 @@ import { Ed25519HdKeyringEntry } from "./ed25519hd";
 
 // Set here for Browsers until this can be configured in Karma
 // https://github.com/karma-runner/karma-jasmine/pull/211
+// tslint:disable-next-line:no-object-mutation
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30*1000;
 
 describe("Ed25519HdKeyringEntry", () => {
