d2211888f4a01dd597edbb509223ac566386a649
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-08-30 21:23:08 +0200

    Experiment enabling some lint rules for ci.
    Currently this works with `yarn lint -c ./tslint_ci.json`
    but not with yarn build.

--- packages/iov-keycontrol/src/keyring.ts
+++ packages/iov-keycontrol/src/keyring.ts
@@ -81,6 +81,8 @@ export class Keyring {
       const parsedData = JSON.parse(data) as KeyringSerialization;
       this.entries = parsedData.entries.map(Keyring.deserializeKeyringEntry);
     } else {
+      //bad comment
+      console.log("construct without data");
       this.entries = [];
     }
   }
