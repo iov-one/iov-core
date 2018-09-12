d6263ef470ab67ea02e50daa3937b067a294f9df
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-09-12 18:43:00 +0200

    Test semirandom invariance

--- packages/iov-crypto/src/semirandom.spec.ts
+++ packages/iov-crypto/src/semirandom.spec.ts
@@ -33,5 +33,29 @@ describe("Bip39", () => {
     const prng = new SemiRandom("secret");
     const prng2 = new SemiRandom("secret");
     expect(prng.random()).toEqual(prng2.random());
+
+    const prng3 = new SemiRandom();
+    const prng4 = new SemiRandom();
+    expect(prng3.random()).toEqual(prng4.random());
+
+    // but the two seeds are different
+    expect(prng.random()).not.toEqual(prng3.random());
+  });
+
+  it("should have no interference with other prng creation", () => {
+    const prng1 = new SemiRandom("top");
+    const r1 = prng1.random();
+
+    // make something in between
+    const foo = new SemiRandom("foo");
+    foo.random();
+
+    // read the first one again
+    const r2 = prng1.random();
+
+    // now, seed new prng like original and make sure it is the same (unaffected by foo)
+    const prng2 = new SemiRandom("top");
+    expect(prng2.random()).toEqual(r1);
+    expect(prng2.random()).toEqual(r2);
   });
 });
