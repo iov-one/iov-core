cf4845c88e810b8c6f1d0e7c43ae9f56319e3b73
Simon Warta <simon@kullo.net>
2018-09-26 12:31:50 +0200

    Create LiskKeyringEntry

--- packages/iov-lisk/types/index.d.ts
+++ packages/iov-lisk/types/index.d.ts
@@ -1,1 +1,2 @@
 export { liskCodec } from "./liskcodec";
+export { LiskKeyringEntry } from "./liskkeyringentry";
---
+++ packages/iov-lisk/types/liskkeyringentry.d.ts
@@ -0,0 +1,28 @@
+import { PrehashType, SignableBytes } from "@iov/bcp-types";
+import { KeyringEntry, KeyringEntryId, KeyringEntryImplementationIdString, KeyringEntrySerializationString, LocalIdentity, PublicIdentity, ValueAndUpdates } from "@iov/keycontrol";
+import { ChainId, SignatureBytes } from "@iov/tendermint-types";
+export declare class LiskKeyringEntry implements KeyringEntry {
+    static readonly implementationId: KeyringEntryImplementationIdString;
+    static registerWithKeyring(): void;
+    private static readonly idsPrng;
+    private static generateId;
+    private static identityId;
+    private static algorithmFromString;
+    private static buildLocalIdentity;
+    readonly implementationId: KeyringEntryImplementationIdString;
+    readonly id: KeyringEntryId;
+    readonly label: ValueAndUpdates<string | undefined>;
+    readonly canSign: ValueAndUpdates<boolean>;
+    private readonly labelProducer;
+    private readonly identities;
+    private readonly passphrases;
+    constructor(data?: KeyringEntrySerializationString);
+    getIdentities(): ReadonlyArray<LocalIdentity>;
+    setLabel(label: string | undefined): void;
+    createIdentity(passphrase?: string): Promise<LocalIdentity>;
+    setIdentityLabel(identity: PublicIdentity, label: string | undefined): void;
+    serialize(): KeyringEntrySerializationString;
+    clone(): KeyringEntry;
+    createTransactionSignature(identity: PublicIdentity, transactionBytes: SignableBytes, prehashType: PrehashType, _: ChainId): Promise<SignatureBytes>;
+    private passphraseForIdentity;
+}
