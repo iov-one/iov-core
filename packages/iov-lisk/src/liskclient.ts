bd268bb83439f2291b439e5f1536cd4e6f258b62
Simon Warta <simon@kullo.net>
2018-09-27 17:12:24 +0200

    Increase sleep time in postTx

--- packages/iov-lisk/src/liskclient.ts
+++ packages/iov-lisk/src/liskclient.ts
@@ -61,7 +61,7 @@ export class LiskClient implements IovReader {
 
     // Sleep some seconds to ensure transaction will be found.
     // Sleep duration determined by trial and error. 15 seconds was not enough.
-    await new Promise(resolve => setTimeout(resolve, 18 * 1000));
+    await new Promise(resolve => setTimeout(resolve, 20 * 1000));
 
     const result = await axios.get(this.baseUrl + `/api/transactions?id=${transactionId}`);
     const responseBody = result.data;
