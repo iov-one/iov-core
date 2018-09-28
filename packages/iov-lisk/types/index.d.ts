757979d44d2044945fda4f900f58ad35e14e1e9c
Simon Warta <simon@kullo.net>
2018-09-28 15:26:21 +0200

    Expose LiskClient for cli testing

--- packages/iov-lisk/types/index.d.ts
+++ packages/iov-lisk/types/index.d.ts
@@ -1,4 +1,4 @@
 export { liskCodec } from "./liskcodec";
 export { LiskKeyringEntry } from "./liskkeyringentry";
 export { liskConnector } from "./liskconnector";
-export { generateNonce } from "./liskclient";
+export { generateNonce, LiskClient } from "./liskclient";
