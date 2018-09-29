7484a23dca918285fa4dbf8562cec6f745711001
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-09-29 21:06:04 +0200

    Allow blockchain scripts to work from any pwd

--- scripts/blockchain_start.sh
+++ scripts/blockchain_start.sh
@@ -1,6 +1,11 @@
 # blockchain_start is called in .travis.yml or also locally before running tests
 # to ensure we have all the blockchains set up for the full integration tests
 
+# get this files directory regardless of pwd when we run it
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# this is root of all the lerna packages to find package-specific scripts
+PACKAGES="${DIR}/../packages"
+
 export TM_VERSION=0.21.0
 export BOV_VERSION=v0.6.0
 docker pull tendermint/tendermint:${TM_VERSION}
@@ -14,15 +19,15 @@ export BOV_DIR=$(mktemp -d "${TMPDIR:-/tmp}/bov_${BOV_VERSION}.XXXXXXXXX")
 echo "TM_DIR = $TM_DIR"
 echo "BOV_DIR = $BOV_DIR"
 
-./packages/iov-tendermint-rpc/tendermint.sh > /tmp/foo.log &
+${PACKAGES}/iov-tendermint-rpc/tendermint.sh > /tmp/foo.log &
 export TM_PID=$!
 echo "Started tendermint" $TM_PID
 sleep 2
 
-./packages/iov-bns/bov_init.sh
-./packages/iov-bns/bov_tm.sh > /tmp/bov_tm.log &
+${PACKAGES}/iov-bns/bov_init.sh
+${PACKAGES}/iov-bns/bov_tm.sh > /tmp/bov_tm.log &
 export BOV_TM_PID=$!
-./packages/iov-bns/bov_app.sh > /tmp/bov_app.log &
+${PACKAGES}/iov-bns/bov_app.sh > /tmp/bov_app.log &
 export BOV_APP_PID=$!
 sleep 3
 # for debug output
