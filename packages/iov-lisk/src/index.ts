309f4f7744f1adaa298f9e13fe046e9c8e19c8d6
Simon Warta <simon@kullo.net>
2018-10-02 13:40:10 +0200

    Remove obsolete LiskKeyringEntry

--- packages/iov-lisk/src/index.ts
+++ packages/iov-lisk/src/index.ts
@@ -1,5 +1,4 @@
 export { passphraseToKeypair } from "./derivation";
 export { liskCodec } from "./liskcodec";
-export { LiskKeyringEntry } from "./liskkeyringentry";
 export { generateNonce, LiskConnection } from "./liskconnection";
 export { liskConnector } from "./liskconnector";
