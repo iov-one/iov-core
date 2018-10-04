370034c42b5f82c8c24873570990dcb486e437f2
Simon Warta <simon@kullo.net>
2018-10-04 09:09:12 +0200

    Don't use SignableBytes as output type of prehash()
    this is confusing since SignableBytes should either be prehashed or
    plain

--- packages/iov-keycontrol/types/prehashing.d.ts
+++ packages/iov-keycontrol/types/prehashing.d.ts
@@ -1,2 +1,2 @@
 import { PrehashType, SignableBytes } from "@iov/bcp-types";
-export declare function prehash(bytes: SignableBytes, type: PrehashType): SignableBytes;
+export declare function prehash(bytes: SignableBytes, type: PrehashType): Uint8Array;
