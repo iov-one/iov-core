7e6f5e8b48cbb0e6eac216eeb8d9d27eaf6030ff
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-10-10 15:28:44 +0200

    Update typescript to 3.1.x

--- packages/iov-crypto/types/ripemd160.d.ts
+++ packages/iov-crypto/types/ripemd160.d.ts
@@ -1,6 +1,6 @@
 import { HashFunction } from "./sha";
 export declare class Ripemd160 implements HashFunction {
-    readonly blockSize: number;
+    readonly blockSize = 64;
     private readonly impl;
     constructor(firstData?: Uint8Array);
     update(data: Uint8Array): Ripemd160;
