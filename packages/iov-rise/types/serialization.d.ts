31ae496b975d9e5dc8accda5255ee35882470228
Simon Warta <simon@kullo.net>
2018-10-18 17:53:57 +0200

    Use Uint64 in public interfaces of @iov/rise

--- packages/iov-rise/types/serialization.d.ts
+++ packages/iov-rise/types/serialization.d.ts
@@ -1,7 +1,7 @@
-import Long from "long";
 import { ReadonlyDate } from "readonly-date";
 import { FullSignature, TransactionIdBytes, UnsignedTransaction } from "@iov/bcp-types";
+import { Uint64 } from "@iov/encoding";
 export declare function toRiseTimestamp(date: ReadonlyDate): number;
-export declare function amountFromComponents(whole: number, fractional: number): Long;
+export declare function amountFromComponents(whole: number, fractional: number): Uint64;
 export declare function serializeTransaction(unsigned: UnsignedTransaction, creationTime: ReadonlyDate): Uint8Array;
 export declare function transactionId(unsigned: UnsignedTransaction, creationTime: ReadonlyDate, primarySignature: FullSignature): TransactionIdBytes;
