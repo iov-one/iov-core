2ae99aa99e875df25a3bdb6a6d51c2dfdb646f0f
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-24 11:25:48 +0200

    Add isValidAddress to lisk codec

--- packages/iov-lisk/src/derivation.spec.ts
+++ packages/iov-lisk/src/derivation.spec.ts
@@ -1,6 +1,6 @@
 import { Encoding } from "@iov/encoding";
 
-import { passphraseToKeypair, pubkeyToAddress } from "./derivation";
+import { isValidAddress, passphraseToKeypair, pubkeyToAddress } from "./derivation";
 
 const { fromHex } = Encoding;
 
@@ -30,6 +30,30 @@ describe("Derivation", () => {
       // https://testnet-explorer.lisk.io/address/10176009299933723198L
       const pubkey = fromHex("06ad4341a609af2de837e1156f81849b05bf3c280940a9f45db76d09a3a3f2fa");
       expect(pubkeyToAddress(pubkey)).toEqual("10176009299933723198L");
+    });
+  });
+
+  describe("isValidAddress", () => {
+    it("works for valid numbers within unsigned int64 range", () => {
+      expect(isValidAddress("1234567890L")).toBeTruthy();
+      expect(isValidAddress("6076671634347365051L")).toBeTruthy();
+      expect(isValidAddress("10176009299933723198L")).toBeTruthy();
+      // 2**64-1
+      expect(isValidAddress("18446744073709551615L")).toBeTruthy();
+    });
+
+    it("rejects malformed addresses", () => {
+      // invalid ending
+      expect(isValidAddress("1234567890R")).toBeFalsy();
+      // leading 0
+      expect(isValidAddress("01234567821L")).toBeFalsy();
+      // decimal
+      expect(isValidAddress("12345.6788L")).toBeFalsy();
+      // string values
+      expect(isValidAddress("12some45L")).toBeFalsy();
+      // 2**64, 2**64 + 1
+      expect(isValidAddress("18446744073709551616L")).toBeFalsy();
+      expect(isValidAddress("18446744073709551617L")).toBeFalsy();
     });
   });
 });
