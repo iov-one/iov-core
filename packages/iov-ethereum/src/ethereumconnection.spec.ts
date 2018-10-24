69f5b91c2d649e25095682f018d3bcde4f22a504
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-10-25 00:34:46 +0200

    Add ETHEREUM_ENABLED to turn on-off tests against node

--- packages/iov-ethereum/src/ethereumconnection.spec.ts
+++ packages/iov-ethereum/src/ethereumconnection.spec.ts
@@ -1,23 +1,34 @@
 import { EthereumConnection } from "./ethereumconnection";
 import { TestConfig } from "./testconfig";
 
+const skipTests = (): boolean => !process.env.ETHEREUM_ENABLED;
+
+const pendingWithoutEthereum = () => {
+  if (skipTests()) {
+    pending("Set ETHEREUM_ENABLED to enable ethereum-node-based tests");
+  }
+};
+
 describe("EthereumConnection", () => {
   const base = TestConfig.base;
   const nodeChainId = TestConfig.chainId;
   const minHeight = TestConfig.minHeight;
 
   it(`can be constructed for ${base}`, () => {
+    pendingWithoutEthereum();
     const connection = new EthereumConnection(base, nodeChainId);
     expect(connection).toBeTruthy();
   });
 
   it("can get chain ID", async () => {
+    pendingWithoutEthereum();
     const connection = await EthereumConnection.establish(base);
     const chainId = connection.chainId();
     expect(chainId).toEqual(nodeChainId);
   });
 
   it("can get height", async () => {
+    pendingWithoutEthereum();
     const connection = await EthereumConnection.establish(base);
     const height = await connection.height();
     expect(height).toBeGreaterThan(minHeight);
