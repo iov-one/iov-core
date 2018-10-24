8a130ccb99c754c7dce267c305f164178ed37e5b
Simon Warta <simon@kullo.net>
2018-10-24 09:27:40 +0200

    Add description to Ed25519.makeKeypair

--- packages/iov-crypto/types/libsodium.d.ts
+++ packages/iov-crypto/types/libsodium.d.ts
@@ -22,6 +22,14 @@ export declare class Ed25519Keypair {
     toLibsodiumPrivkey(): Uint8Array;
 }
 export declare class Ed25519 {
+    /**
+     * Generates a keypair deterministically from a given 32 bytes seed.
+     *
+     * This seed equals the Ed25519 private key.
+     * For implementation details see crypto_sign_seed_keypair in
+     * https://download.libsodium.org/doc/public-key_cryptography/public-key_signatures.html
+     * and diagram on https://blog.mozilla.org/warner/2011/11/29/ed25519-keys/
+     */
     static makeKeypair(seed: Uint8Array): Promise<Ed25519Keypair>;
     static createSignature(message: Uint8Array, keyPair: Ed25519Keypair): Promise<Uint8Array>;
     static verifySignature(signature: Uint8Array, message: Uint8Array, pubkey: Uint8Array): Promise<boolean>;
