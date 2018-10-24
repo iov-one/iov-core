e9180f1c30a62d0752d245448bb8c25e35a79698
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-10-24 14:04:17 +0200

    Use regular exp for hex match

--- packages/iov-ethereum/src/utils.ts
+++ packages/iov-ethereum/src/utils.ts
@@ -1,7 +1,5 @@
 export function decodeHexQuantity(hexString: string): number {
-  if (hexString === "0x0" || hexString === "0x00") {
-    return 0;
-  } else if (hexString.indexOf("0x") === 0 && hexString.indexOf("0x0") !== 0 && hexString.length > 2) {
+  if (hexString.match(/^0x[a-f0-9]+$/)) {
     return parseInt(hexString, 16);
   }
   throw new Error("invalid hex quantity input");
