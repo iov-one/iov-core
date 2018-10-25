43d15159a7a8ad9568448a86e24cf553124e42ca
Simon Warta <simon@kullo.net>
2018-10-25 14:46:34 +0200

    Deduplicate tendermint and BOV version numbers

--- packages/iov-tendermint-rpc/tendermint.sh
+++ packages/iov-tendermint-rpc/tendermint.sh
@@ -2,18 +2,27 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
+if [ -z "$TM_VERSION" ]; then
+  echo "TM_VERSION must be set"
+  exit 1
+fi
+
+if [ -z "$TM_DIR" ]; then
+  echo "TM_DIR must be set"
+  exit 1
+fi
+
 PORT=${TM_PORT:-12345}
-VERSION=${TM_VERSION:-0.21.0}
 
 chmod 777 "${TM_DIR}"
 
 docker run -v "${TM_DIR}:/tendermint" \
-  "tendermint/tendermint:${VERSION}" init
+  "tendermint/tendermint:${TM_VERSION}" init
 
 # must enable tx index for search and subscribe
 exec docker run -p "${PORT}:26657" -v "${TM_DIR}:/tendermint" \
   -e "TM_TX_INDEX_INDEX_ALL_TAGS=true" \
-  "tendermint/tendermint:${VERSION}" node \
+  "tendermint/tendermint:${TM_VERSION}" node \
   --proxy_app=kvstore \
   --rpc.laddr=tcp://0.0.0.0:26657 \
   --log_level=state:info,rpc:info,*:error
