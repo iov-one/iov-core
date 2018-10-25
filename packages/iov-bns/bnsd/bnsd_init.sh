cdd1950c00d63e4467f8ef26e3093088144f9936
Simon Warta <simon@kullo.net>
2018-10-26 00:18:48 +0200

    Use custom genesis file

--- packages/iov-bns/bnsd/bnsd_init.sh
+++ packages/iov-bns/bnsd/bnsd_init.sh
@@ -14,10 +14,18 @@ if [ -z "$BOV_DIR" ]; then
   echo "BOV_DIR must be set"; exit 1
 fi
 
+
 chmod 777 "${BOV_DIR}"
 
-docker run -v "${BOV_DIR}:/tendermint" \
-  "iov1/tendermint:${TM_VERSION}" init
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p "${BOV_DIR}/config"
+cp "$SCRIPT_DIR/genesis.json" "${BOV_DIR}/config/genesis.json"
 
-docker run -v "${BOV_DIR}:/data" "iov1/bnsd:${BOV_VERSION}" -home "/data" \
-  init CASH b1ca7e78f74423ae01da3b51e676934d9105f282
+docker run -v "${BOV_DIR}:/tendermint" \
+  "iov1/tendermint:${TM_VERSION}" \
+  init
+
+docker run -v "${BOV_DIR}:/data" \
+  "iov1/bnsd:${BOV_VERSION}" \
+  -home "/data" \
+  init -i
