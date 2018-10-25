fc9804cde6532604e120df1fc17f52cb0b7a64a6
Simon Warta <simon@kullo.net>
2018-10-25 14:59:06 +0200

    Run shellcheck on start/stopGanache.sh

--- packages/iov-ethereum/startGanache.sh
+++ packages/iov-ethereum/startGanache.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+command -v shellcheck > /dev/null && shellcheck "$0"
 
 function startupGanache() {
+  # shellcheck disable=SC2009
   GANACHE_SEARCH="$(ps -ef | grep "[g]anache" | awk '{print $2}')"
   GANACHE_PID="${GANACHE_SEARCH}"
   if [ "$GANACHE_PID" != "" ]
   then
     echo "Killing existing Ganache CLI process $GANACHE_PID"
-    kill -9 $GANACHE_PID
+    kill -9 "$GANACHE_PID"
   fi
   ./node_modules/.bin/ganache-cli -p 7545 -i 5777 -m "$MNEMONIC" > /dev/null &
   GANACHE_PID=$!
--- packages/iov-ethereum/stopGanache.sh
+++ packages/iov-ethereum/stopGanache.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Helper script for starting/stopping ganache and running tests
-#
+command -v shellcheck > /dev/null && shellcheck "$0"
 
 function shutdownGanache() {
+  # shellcheck disable=SC2009
   GANACHE_SEARCH="$(ps -ef | grep "[g]anache" | awk '{print $2}')"
   GANACHE_PID="${GANACHE_SEARCH}"
   if [ "$GANACHE_PID" != "" ]
   then
     echo "Killing existing Ganache CLI process $GANACHE_PID"
-    kill -9 $GANACHE_PID
+    kill -9 "$GANACHE_PID"
   fi
 }
 
