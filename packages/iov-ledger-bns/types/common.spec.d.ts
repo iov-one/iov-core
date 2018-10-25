b3ee74cb7c70858319ab671e65d801aed7c54f71
Simon Warta <simon@kullo.net>
2018-10-25 17:00:02 +0200

    Update @iov/ledger-bns types

--- packages/iov-ledger-bns/types/common.spec.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const skipTests: () => boolean;
-export declare const pendingWithoutLedger: () => void;
-export declare const skipSeededTests: () => boolean;
-export declare const pendingWithoutSeededLedger: () => void;
-export declare const skipInteractiveTests: () => boolean;
-export declare const pendingWithoutInteractiveLedger: () => void;
