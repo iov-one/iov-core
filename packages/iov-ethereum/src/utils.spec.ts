dea503ea1f7c2185bad742a75f0c74b9770a8036
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-10-26 12:34:58 +0200

    Add decodeHexQuantityString utils

--- packages/iov-ethereum/src/utils.spec.ts
+++ packages/iov-ethereum/src/utils.spec.ts
@@ -1,4 +1,4 @@
-import { decodeHexQuantity } from "./utils";
+import { decodeHexQuantity, decodeHexQuantityString } from "./utils";
 
 describe("Ethereum utils", () => {
   describe("decodeHexQuantity", () => {
@@ -19,7 +19,35 @@ describe("Ethereum utils", () => {
     it("throws error for invalid inputs", () => {
       expect(() => decodeHexQuantity("0x")).toThrowError(/invalid hex quantity input/);
       expect(() => decodeHexQuantity("ff")).toThrowError(/invalid hex quantity input/);
-      expect(() => decodeHexQuantity("0xA0xA0xA0xA0xA")).toThrowError(/invalid hex quantity input/);
+      expect(() => decodeHexQuantity("0xa0xa0xa0xa0xa")).toThrowError(/invalid hex quantity input/);
+    });
+  });
+
+  describe("decodeHexQuantityString", () => {
+    it("verify valid inputs", () => {
+      let decStrQty;
+      decStrQty = decodeHexQuantityString("0xd3c21bcecceda0ffffff");
+      expect(decStrQty).toEqual("999999999999999999999999");
+      decStrQty = decodeHexQuantityString("0xde0b6b3a763ffff");
+      expect(decStrQty).toEqual("999999999999999999");
+      decStrQty = decodeHexQuantityString("0x38d7ea4c67fff");
+      expect(decStrQty).toEqual("999999999999999");
+      decStrQty = decodeHexQuantityString("0x09184e72a000");
+      expect(decStrQty).toEqual("10000000000000");
+      decStrQty = decodeHexQuantityString("0x400");
+      expect(decStrQty).toEqual("1024");
+      decStrQty = decodeHexQuantityString("0x41");
+      expect(decStrQty).toEqual("65");
+      decStrQty = decodeHexQuantityString("0x2");
+      expect(decStrQty).toEqual("2");
+      decStrQty = decodeHexQuantityString("0x0");
+      expect(decStrQty).toEqual("0");
+    });
+
+    it("throws error for invalid inputs", () => {
+      expect(() => decodeHexQuantity("0x")).toThrowError(/invalid hex quantity input/);
+      expect(() => decodeHexQuantity("ff")).toThrowError(/invalid hex quantity input/);
+      expect(() => decodeHexQuantity("0xa0xa0xa0xa0xa")).toThrowError(/invalid hex quantity input/);
     });
   });
 });
--- packages/iov-ethereum/types/utils.d.ts
+++ packages/iov-ethereum/types/utils.d.ts
@@ -1,1 +1,2 @@
 export declare function decodeHexQuantity(hexString: string): number;
+export declare function decodeHexQuantityString(hexString: string): string;
