25a5d2dee0963c668b9ff52c1ef62baf7e014079
Simon Warta <simon@kullo.net>
2018-11-07 13:39:22 +0100

    Update weave downloader script

--- packages/iov-bns/scripts/weave.sh
+++ packages/iov-bns/scripts/weave.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
 # This will check out the version of weave passed on the cli.
-# Usage: bash weave.sh <v0.9.0>
-# OR     VERSION=v0.9.0 bash weave.sh
+# Usage: ./scripts/weave.sh v0.9.0
+# OR     VERSION=v0.9.0 ./scripts/weave.sh
 
 # use cli arg, or environmental variable
 TAG=${1:-$VERSION}
 if [ -z "$TAG" ]; then
-  echo "Please provide a tag to checkout, either as an arg or via VERSION env var";
-  exit 1;
+  echo "Please provide a tag to checkout, either as an arg or via VERSION env var"
+  exit 1
 fi
 
 # clone or update weave
 if [ -d weave ]; then
-  cd weave;
-  git fetch;
+  (
+    cd weave
+    git fetch
+    git checkout "$TAG"
+  )
 else
-  git clone git@github.com:iov-one/weave.git
-  cd weave;
+  git clone --branch "$TAG" git@github.com:iov-one/weave.git
 fi
-git checkout "$TAG";
