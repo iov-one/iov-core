df5c828be343ee14966b39c0c0c5d57f7e2f1246
Simon Warta <simon@kullo.net>
2018-11-08 18:38:51 +0100

    Start faucet via docker

--- scripts/iov_faucet_start.sh
+++ scripts/iov_faucet_start.sh
@@ -2,18 +2,25 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
-TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/faucet_start.XXXXXXXXX")
-(
-  cd "$TMP_DIR"
-  git clone --depth 1 --branch master https://github.com/iov-one/iov-faucet
-  (
-    cd "iov-faucet"
-    yarn install
-    yarn build
+# Coose from https://hub.docker.com/r/iov1/iov-faucet/tags/
+FAUCET_VERSION="v0.1.0"
 
-    LOGFILE="$TMP_DIR/faucet.log"
-    yarn dev-start > "$LOGFILE" &
-    FAUCET_PID=$!
-    echo "Faucet running as process $FAUCET_PID logging into $LOGFILE"
-  )
-)
+TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/faucet_start.XXXXXXXXX")
+LOGFILE="$TMP_DIR/faucet.log"
+
+docker pull "alpine"
+DOCKER_HOST_IP=$(docker run --rm alpine ip route | awk 'NR==1 {print $3}')
+
+BLOCKCHAIN_URL="ws://$DOCKER_HOST_IP:22345"
+echo "Connecting to $BLOCKCHAIN_URL"
+
+docker pull "iov1/iov-faucet:${FAUCET_VERSION}"
+docker run --read-only \
+  --env "FAUCET_CONCURRENCY=3" \
+  --env "FAUCET_MNEMONIC=degree tackle suggest window test behind mesh extra cover prepare oak script"  \
+  -p 8000:8000 \
+  --rm "iov1/iov-faucet:${FAUCET_VERSION}" \
+  start bns "$BLOCKCHAIN_URL" \
+  > "$LOGFILE" &
+
+echo "Faucet running and logging into $LOGFILE"
