d887b57f07e51e1cb1a6c677d2c5aaa07b419430
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-11-12 14:41:55 -0300

    Add encodeQuantity function

--- packages/iov-ethereum/types/utils.d.ts
+++ packages/iov-ethereum/types/utils.d.ts
@@ -2,3 +2,4 @@ import { Nonce } from "@iov/bcp-types";
 export declare function decodeHexQuantity(hexString: string): number;
 export declare function decodeHexQuantityString(hexString: string): string;
 export declare function decodeHexQuantityNonce(hexString: string): Nonce;
+export declare function encodeQuantity(value: number): string;
