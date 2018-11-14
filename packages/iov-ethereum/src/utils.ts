7bd2b870b3ab7cc6bc729757727f58db35e397a5
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-11-14 14:11:15 -0300

    Fix test for ganache 0x00 response

--- packages/iov-ethereum/src/utils.ts
+++ packages/iov-ethereum/src/utils.ts
@@ -4,7 +4,8 @@ import { Int53 } from "@iov/encoding";
 import BN = require("bn.js");
 
 export function decodeHexQuantity(hexString: string): number {
-  if (hexString === "0x0" || hexString.match(/^0x{1}[a-f1-9][a-f0-9]*$/)) {
+  // "0x00" in ganache response is an error: zero should be represented as "0x0"
+  if (hexString === "0x0" || hexString === "0x00" || hexString.match(/^0x{1}[a-f1-9][a-f0-9]*$/)) {
     return parseInt(hexString, 16);
   }
   throw new Error("invalid hex quantity input");
