d9090354f75d5f3867aea6e446adfd066ec28315
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-11-16 00:38:17 -0300

    Add bytesToPost function

--- packages/iov-ethereum/src/ethereumcodec.ts
+++ packages/iov-ethereum/src/ethereumcodec.ts
@@ -7,17 +7,20 @@ import {
   SignedTransaction,
   SigningJob,
   TransactionIdBytes,
+  TransactionKind,
   TxCodec,
   UnsignedTransaction,
 } from "@iov/bcp-types";
 import { Keccak256 } from "@iov/crypto";
 import { Encoding } from "@iov/encoding";
 
+import { encodeQuantity, encodeQuantityString, hexPadToEven, stringDataToHex } from "./utils";
+
 // import { constants } from "./constants";
 import { isValidAddress } from "./derivation";
 import { Serialization } from "./serialization";
 
-const { toAscii, toHex } = Encoding;
+const { fromHex, toAscii, toHex } = Encoding;
 
 export function toChecksumAddress(address: string): Address {
   // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
@@ -38,7 +41,72 @@ export const ethereumCodec: TxCodec = {
     };
   },
   bytesToPost: (signed: SignedTransaction): PostableBytes => {
-    throw new Error(`Not implemented tx: ${signed}`);
+    switch (signed.transaction.kind) {
+      case TransactionKind.Send:
+        let gasPriceHex = "0x";
+        let gasLimitHex = "0x";
+        let dataHex = "0x";
+        let nonceHex = "0x";
+
+        const valueHex = encodeQuantityString(
+          Serialization.amountFromComponents(
+            signed.transaction.amount.whole,
+            signed.transaction.amount.fractional,
+          ),
+        );
+        if (signed.primarySignature.nonce.toNumber() > 0) {
+          nonceHex = encodeQuantity(signed.primarySignature.nonce.toNumber());
+        }
+        if (signed.transaction.gasPrice) {
+          gasPriceHex = encodeQuantityString(
+            Serialization.amountFromComponents(
+              signed.transaction.gasPrice.whole,
+              signed.transaction.gasPrice.fractional,
+            ),
+          );
+        }
+        if (signed.transaction.gasLimit) {
+          gasLimitHex = encodeQuantityString(
+            Serialization.amountFromComponents(
+              signed.transaction.gasLimit.whole,
+              signed.transaction.gasLimit.fractional,
+            ),
+          );
+        }
+        if (signed.transaction.memo) {
+          dataHex = stringDataToHex(signed.transaction.memo);
+        }
+        if (!isValidAddress(signed.transaction.recipient)) {
+          throw new Error("Invalid recipient address");
+        }
+        const sig = signed.primarySignature.signature;
+        // FIXME: r and s are not working: invalid signature
+        const r = sig.slice(0, 32);
+        const s = sig.slice(32, 64);
+        // TODO: find the right way to do this
+        let v = Number(sig.slice(64, 65)) - 254 + 27;
+        const chainId = Number(signed.transaction.chainId);
+        if (chainId > 0) {
+          v += chainId * 2 + 8;
+        }
+        const chainIdHex = encodeQuantity(v);
+        const postableTx = new Uint8Array(
+          Encoding.toRlp([
+            Buffer.from(fromHex(hexPadToEven(nonceHex))),
+            Buffer.from(fromHex(hexPadToEven(gasPriceHex))),
+            Buffer.from(fromHex(hexPadToEven(gasLimitHex))),
+            Buffer.from(fromHex(hexPadToEven(signed.transaction.recipient))),
+            Buffer.from(fromHex(hexPadToEven(valueHex))),
+            Buffer.from(fromHex(hexPadToEven(dataHex))),
+            Buffer.from(fromHex(hexPadToEven(chainIdHex))),
+            Buffer.from(r),
+            Buffer.from(s),
+          ]),
+        );
+        return postableTx as PostableBytes;
+      default:
+        throw new Error("Unsupported kind of transaction");
+    }
   },
   identifier: (signed: SignedTransaction): TransactionIdBytes => {
     throw new Error(`Not implemented tx: ${signed}`);
