773d2154a3db7843a2627f939dd52010a9068a29
LucasIsasmendi <lucas.isasmendi@gmail.com>
2018-11-17 23:46:44 -0300

    Improve test and comments for malformed addresses

--- packages/iov-ethereum/src/derivation.ts
+++ packages/iov-ethereum/src/derivation.ts
@@ -5,16 +5,19 @@ import { Encoding } from "@iov/encoding";
 const { toAscii, toHex } = Encoding;
 
 export function isValidAddress(address: string): boolean {
-  // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
-  const addressLower = address.toLowerCase().replace("0x", "") as Address;
-  const addressHash = toHex(new Keccak256(toAscii(addressLower)).digest());
-  for (let i = 0; i < 40; i++) {
-    if (
-      (parseInt(addressHash[i], 16) > 7 && addressLower[i].toUpperCase() !== address[i + 2]) ||
-      (parseInt(addressHash[i], 16) <= 7 && addressLower[i] !== address[i + 2])
-    ) {
-      return false;
+  if (address.match(/^0x[a-fA-F0-9]+$/) && address.length === 42) {
+    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
+    const addressLower = address.toLowerCase().replace("0x", "") as Address;
+    const addressHash = toHex(new Keccak256(toAscii(addressLower)).digest());
+    for (let i = 0; i < 40; i++) {
+      if (
+        (parseInt(addressHash[i], 16) > 7 && addressLower[i].toUpperCase() !== address[i + 2]) ||
+        (parseInt(addressHash[i], 16) <= 7 && addressLower[i] !== address[i + 2])
+      ) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
+  throw new Error("Invalid ethereum address");
 }
