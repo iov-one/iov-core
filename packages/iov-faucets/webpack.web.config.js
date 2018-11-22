fd3c201dfee397040e10e4462bc4a388955b0e0b
Simon Warta <simon@kullo.net>
2018-11-22 10:22:19 +0100

    Pass FAUCET_ENABLED to faucet tests

--- packages/iov-faucets/webpack.web.config.js
+++ packages/iov-faucets/webpack.web.config.js
@@ -1,5 +1,6 @@
 const glob = require('glob');
 const path = require('path');
+const webpack = require('webpack');
 
 const target = "web";
 const distdir = path.join(__dirname, "dist", "web");
@@ -13,5 +14,8 @@ module.exports = [
       path: distdir,
       filename: "tests.js",
     },
+    plugins: [
+      new webpack.EnvironmentPlugin(['FAUCET_ENABLED']),
+    ],
   },
 ];
