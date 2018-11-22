7d1f06fef00aa8435bd67e602f576a44098a1bcb
Simon Warta <simon@kullo.net>
2018-11-22 13:07:19 +0100

    Simplify streamPromise implementation

--- packages/iov-stream/types/promise.d.ts
+++ packages/iov-stream/types/promise.d.ts
@@ -1,2 +1,5 @@
 import { Stream } from "xstream";
+/**
+ * Emits one event for each array element as soon as the promise resolves
+ */
 export declare function streamPromise<T>(promise: Promise<Iterable<T>>): Stream<T>;
