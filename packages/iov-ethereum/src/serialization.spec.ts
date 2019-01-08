9bc4a86c3248d7889522600c31e9bd401d897544
Simon Warta <simon@kullo.net>
2019-01-08 12:38:40 +0100

    Make serialization tests independent of configured test network

--- packages/iov-ethereum/src/serialization.spec.ts
+++ packages/iov-ethereum/src/serialization.spec.ts
@@ -13,7 +13,6 @@ import { ExtendedSecp256k1Signature } from "@iov/crypto";
 import { Encoding, Int53 } from "@iov/encoding";
 
 import { Serialization } from "./serialization";
-import { testConfig } from "./testconfig.spec";
 
 const { serializeSignedTransaction, serializeUnsignedTransaction } = Serialization;
 const { fromHex } = Encoding;
@@ -27,7 +26,7 @@ describe("Serialization", () => {
 
       const tx: SendTransaction = {
         kind: "bcp/send",
-        chainId: testConfig.chainId,
+        chainId: "ethereum-eip155-5777" as ChainId,
         signer: {
           algo: Algorithm.Secp256k1,
           data: pubkey as PublicKeyBytes,
@@ -49,7 +48,8 @@ describe("Serialization", () => {
         },
         recipient: "0x43aa18FAAE961c23715735682dC75662d90F4DDe" as Address,
       };
-      const serializedTx = serializeUnsignedTransaction(tx, testConfig.nonce);
+      const nonce = new Int53(0) as Nonce;
+      const serializedTx = serializeUnsignedTransaction(tx, nonce);
       expect(serializedTx).toEqual(
         fromHex(
           "ef808504a817c8008252089443aa18faae961c23715735682dc75662d90f4dde8901158e460913d00000808216918080",
@@ -64,7 +64,7 @@ describe("Serialization", () => {
 
       const tx: SendTransaction = {
         kind: "bcp/send",
-        chainId: testConfig.chainId,
+        chainId: "ethereum-eip155-5777" as ChainId,
         signer: {
           algo: Algorithm.Secp256k1,
           data: pubkey as PublicKeyBytes,
@@ -88,7 +88,8 @@ describe("Serialization", () => {
         memo:
           "The nice memo I attach to that money for the whole world to read, And can encode as much data as you want, and unicode symbols like \u2764",
       };
-      const serializedTx = serializeUnsignedTransaction(tx, testConfig.nonce);
+      const nonce = new Int53(0) as Nonce;
+      const serializedTx = serializeUnsignedTransaction(tx, nonce);
       expect(serializedTx).toEqual(
         fromHex(
           "f8b7808504a817c8008252089443aa18faae961c23715735682dc75662d90f4dde8901158e460913d00000b887546865206e696365206d656d6f20492061747461636820746f2074686174206d6f6e657920666f72207468652077686f6c6520776f726c6420746f20726561642c20416e642063616e20656e636f6465206173206d756368206461746120617320796f752077616e742c20616e6420756e69636f64652073796d626f6c73206c696b6520e29da48216918080",
