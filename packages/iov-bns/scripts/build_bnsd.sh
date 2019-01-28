9dd02cffde6a7a18938d65bede0a4530d2e9a44e
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-01-28 17:35:53 +0100

    Update from pr feedback

--- packages/iov-bns/scripts/build_bnsd.sh
+++ packages/iov-bns/scripts/build_bnsd.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
 # This depends on an existing version from a previous weave.sh run
 # This will do all steps to build bnsd and copy it into ./go/bin
 
--- packages/iov-bns/scripts/clean.sh
+++ packages/iov-bns/scripts/clean.sh
@@ -4,7 +4,7 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 
 # This must be run after weave.sh and will clean up the protobuf files
 # before compilation, to remove traces of gogoproto
-# 
+#
 # It will write all cleaned proto to ./$OUTDIR, default ./clean
 
 BASEDIR=`pwd`
@@ -14,17 +14,19 @@ OUTDIR=${OUTDIR:-clean}
 
 rm -rf ./${OUTDIR}
 
-cd "$CODEDIR"
-for file in `find . -name '*.proto' -not -path '*/vendor/*' -not -path '*/examples/*' -not -path '*/cmd/bcpd/*'`; do
-  outfile="$BASEDIR/$OUTDIR/$file"
-  outdir=$(dirname $outfile)
-  mkdir -p $outdir
-  echo $outfile
 
-  # This was targeted removing of gogoproto flags, but left []; for options on fields, which caused silent compile errors
-  # cat $file | sed 's/(gogoproto.*) *= *[^]]*//g' | sed 's/^.*github.com\/gogo\/protobuf\/gogoproto\/gogo.*//' > $outfile
+(
+  cd "$CODEDIR"
+  for file in `find . -name '*.proto' -not -path '*/vendor/*' -not -path '*/examples/*' -not -path '*/cmd/bcpd/*'`; do
+    outfile="$BASEDIR/$OUTDIR/$file"
+    outdir=$(dirname $outfile)
+    mkdir -p $outdir
+    echo $outfile
 
-  # This just removes all options after the fields, and removes illegal ;; typos
-  cat $file | sed 's/ *\[[^]]*\];/;/g' | sed 's/;;/;/' > $outfile
-done
-cd -
+    # This was targeted removing of gogoproto flags, but left []; for options on fields, which caused silent compile errors
+    # cat $file | sed 's/(gogoproto.*) *= *[^]]*//g' | sed 's/^.*github.com\/gogo\/protobuf\/gogoproto\/gogo.*//' > $outfile
+
+    # This just removes all options after the fields, and removes illegal ;; typos
+    cat $file | sed 's/ *\[[^]]*\];/;/g' | sed 's/;;/;/' > $outfile
+  done
+)
