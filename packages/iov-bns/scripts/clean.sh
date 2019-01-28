47f5aa4571a4fcd49423fe132623d4be9d1c1f82
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-01-28 18:03:04 +0100

    Shellcheck-linted clean.sh

--- packages/iov-bns/scripts/clean.sh
+++ packages/iov-bns/scripts/clean.sh
@@ -7,26 +7,24 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 #
 # It will write all cleaned proto to ./$OUTDIR, default ./clean
 
-BASEDIR=`pwd`
+BASEDIR=$(pwd)
 
 CODEDIR=go/src/github.com/iov-one/weave
 OUTDIR=${OUTDIR:-clean}
 
-rm -rf ./${OUTDIR}
-
+rm -rf "./${OUTDIR}"
 
 (
   cd "$CODEDIR"
-  for file in `find . -name '*.proto' -not -path '*/vendor/*' -not -path '*/examples/*' -not -path '*/cmd/bcpd/*'`; do
+  find . -name '*.proto' -not -path '*/vendor/*' -not -path '*/examples/*' -not -path '*/cmd/bcpd/*' > tmp
+  while IFS= read -r file
+  do
     outfile="$BASEDIR/$OUTDIR/$file"
-    outdir=$(dirname $outfile)
-    mkdir -p $outdir
-    echo $outfile
-
-    # This was targeted removing of gogoproto flags, but left []; for options on fields, which caused silent compile errors
-    # cat $file | sed 's/(gogoproto.*) *= *[^]]*//g' | sed 's/^.*github.com\/gogo\/protobuf\/gogoproto\/gogo.*//' > $outfile
-
+    outdir=$(dirname "$outfile")
+    mkdir -p "$outdir"
+    echo "$outfile"
     # This just removes all options after the fields, and removes illegal ;; typos
-    cat $file | sed 's/ *\[[^]]*\];/;/g' | sed 's/;;/;/' > $outfile
-  done
+    sed 's/ *\[[^]]*\];/;/g' "$file" | sed 's/;;/;/' > "$outfile"
+  done < tmp
+  rm tmp
 )
