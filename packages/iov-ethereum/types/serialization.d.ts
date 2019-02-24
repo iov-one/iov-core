f1e4fe28cad4b523bbe148807918c96d2b7c7f59
Simon Warta <simon@kullo.net>
2019-02-24 22:04:48 +0100

    Extract serializeUnsignedEthSendTransaction

--- packages/iov-ethereum/types/serialization.d.ts
+++ packages/iov-ethereum/types/serialization.d.ts
@@ -1,5 +1,6 @@
 import { Nonce, SignedTransaction, UnsignedTransaction } from "@iov/bcp-types";
 export declare class Serialization {
+    static serializeUnsignedEthSendTransaction(nonce: Nonce, gasPriceHex: string, gasLimitHex: string, recipientHex: string, valueHex: string, dataHex: string, chainIdHex: string): Uint8Array;
     static serializeUnsignedTransaction(unsigned: UnsignedTransaction, nonce: Nonce): Uint8Array;
     static serializeSignedTransaction(signed: SignedTransaction): Uint8Array;
     /**
