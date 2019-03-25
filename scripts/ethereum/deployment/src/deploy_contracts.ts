9a0d0fc1d06d619ef345a98f1522a38e42490bfe
Simon Warta <simon@kullo.net>
2019-03-25 15:34:26 +0100

    Deploy TrashToken

--- scripts/ethereum/deployment/src/deploy_contracts.ts
+++ scripts/ethereum/deployment/src/deploy_contracts.ts
@@ -1,9 +1,11 @@
 // tslint:disable: no-submodule-imports
 import { Address } from 'web3x/address';
+import { Contract, TxSend } from 'web3x/contract';
 import { Eth } from 'web3x/eth';
 import { WebsocketProvider } from 'web3x/providers';
 
 import { AshToken } from "./AshToken";
+import { TrashToken } from "./TrashToken";
 
 const ganacheHost = `localhost:${process.env.GANACHE_PORT}`;
 const ganacheGasPrice = 50000;
@@ -16,16 +18,31 @@ function debugAddress(address: Address | undefined): string | undefined {
   return address ? address.toString() : undefined;
 }
 
+interface DeployableContract extends Contract<any> {
+  readonly deploy: () => TxSend<any>,
+}
+
+interface DeploymentJob {
+  readonly name: string,
+  readonly contract: DeployableContract,
+}
+
+interface MintingJob {
+  readonly recipient: Address,
+  readonly quantity: string,
+}
+
 async function main(): Promise<void> {
   const provider = new WebsocketProvider(`ws://${ganacheHost}/ws`);
   const eth = new Eth(provider);
 
   // Order matters to get reproducible contract addresses
-  const contracts = new Map([
-    ["AshToken", new AshToken(eth)],
-  ]);
+  const deploymentJobs: ReadonlyArray<DeploymentJob> = [
+    { name: "AshToken", contract: new AshToken(eth), },
+    { name: "TrashToken", contract: new TrashToken(eth), },
+  ];
 
-  for (const [name, contract] of contracts) {
+  for (const {name, contract} of deploymentJobs) {
     const deploymentTransaction = contract.deploy();
 
     const estimatedGas = await deploymentTransaction.estimateGas();
@@ -35,12 +52,12 @@ async function main(): Promise<void> {
     console.log(`${name} deployed to`, debugAddress(receipt.contractAddress));
 
     if (typeof contract.methods.mint !== "undefined") {
-      const mintingJobs = new Map<Address, string>([
-        [secondIdentity, "33445566"],
-        [Address.fromString("0x0000000000111111111122222222223333333333"), "38"],
-      ]);
+      const mintingJobs: ReadonlyArray<MintingJob> = [
+        { recipient: secondIdentity, quantity: "33445566" },
+        { recipient: Address.fromString("0x0000000000111111111122222222223333333333"), quantity: "38" },
+      ];
 
-      for (const [recipient, quantity] of mintingJobs) {
+      for (const {recipient, quantity} of mintingJobs) {
         console.log(`Minting ${quantity} atomic units for ${recipient} ...`);
         await contract.methods.mint(recipient, quantity)
           .send({ from: mainIdentity, gasPrice: ganacheGasPrice })
