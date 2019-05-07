2d5c69f9dd31339f12c291c6aa047da203e32d17
Simon Warta <simon@kullo.net>
2019-05-07 18:03:07 +0200

    Remove re-exports in @iov/core

--- packages/iov-core/src/index.ts
+++ packages/iov-core/src/index.ts
@@ -1,17 +1,3 @@
-export { Address, ChainId, Nonce, SendTransaction, TokenTicker } from "@iov/bcp";
-export {
-  Ed25519HdWallet,
-  Ed25519Wallet,
-  HdPaths,
-  Keyring,
-  Secp256k1HdWallet,
-  UserProfile,
-  Wallet,
-  WalletId,
-  WalletImplementationIdString,
-  WalletSerializationString,
-} from "@iov/keycontrol";
-
 export { JsonRpcSigningServer } from "./jsonrpcsigningserver";
 export { MultiChainSigner } from "./multichainsigner";
 export {
