3455660a7ed00527d3ab530c11cc5eb3732f82fe
Simon Warta <simon@kullo.net>
2019-05-09 09:30:42 +0200

    Upgrade ganache-cli to v6.4.3

--- scripts/ethereum/start.sh
+++ scripts/ethereum/start.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
 # Choose from https://hub.docker.com/r/trufflesuite/ganache-cli/tags
-VERSION="v6.4.1"
+VERSION="v6.4.3"
 PORT="8545"
 
 TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/ganache.XXXXXXXXX")
