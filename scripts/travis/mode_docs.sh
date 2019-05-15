0dafa415bb6db4f15e2adcaaac54b7a81c16c08e
Simon Warta <simon@kullo.net>
2019-05-15 12:55:58 +0200

    Create typedoc binary with TypeScript 3.4 support

--- scripts/travis/mode_docs.sh
+++ scripts/travis/mode_docs.sh
@@ -10,6 +10,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck disable=SC1090
 source "$SCRIPT_DIR/_includes.sh";
 
+export PATH="${SCRIPT_DIR}/../typedoc/bin:${PATH}"
+
 #
 # Build
 #
