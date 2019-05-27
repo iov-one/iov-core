b41876a4a35096c668a21af78c3aca1fd65ab3cd
Simon Warta <simon@kullo.net>
2019-05-27 12:15:15 +0200

    Convert test to: Full atomic swap between BNS and BNS

--- packages/iov-core/src/integrationtests/atomicswapbnsbcp.spec.ts
+++ packages/iov-core/src/integrationtests/atomicswapbnsbcp.spec.ts
@@ -27,7 +27,7 @@ import { Ed25519HdWallet, HdPaths, UserProfile } from "@iov/keycontrol";
 import { MultiChainSigner } from "../multichainsigner";
 
 const CASH = "CASH" as TokenTicker;
-const MASH = "MASH" as TokenTicker;
+const BASH = "BASH" as TokenTicker;
 
 async function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -44,18 +44,10 @@ function pendingWithoutBnsd(): void {
   }
 }
 
-function pendingWithoutBcpd(): void {
-  if (!process.env.BCPD_ENABLED) {
-    pending("Set BCPD_ENABLED to enable bcpd-based tests");
-  }
-}
-
 interface ActorData {
   readonly signer: MultiChainSigner;
   readonly bnsConnection: AtomicSwapConnection;
-  readonly bcpConnection: AtomicSwapConnection;
   readonly bnsIdentity: PublicIdentity;
-  readonly bcpIdentity: PublicIdentity;
 }
 
 class Actor {
@@ -65,41 +57,30 @@ class Actor {
     const signer = new MultiChainSigner(profile);
 
     const bnsConnection = (await signer.addChain(bnsConnector("ws://localhost:23456"))).connection;
-    const bcpConnection = (await signer.addChain(bnsConnector("ws://localhost:23457"))).connection;
 
     const bnsIdentity = await profile.createIdentity(wallet.id, bnsConnection.chainId(), hdPath);
-    const bcpIdentity = await profile.createIdentity(wallet.id, bcpConnection.chainId(), hdPath);
 
     return new Actor({
       signer: signer,
       bnsConnection: bnsConnection as AtomicSwapConnection,
-      bcpConnection: bcpConnection as AtomicSwapConnection,
       bnsIdentity: bnsIdentity,
-      bcpIdentity: bcpIdentity,
     });
   }
 
   public readonly bnsIdentity: PublicIdentity;
-  public readonly bcpIdentity: PublicIdentity;
   public get bnsAddress(): Address {
     return this.signer.identityToAddress(this.bnsIdentity);
-  }
-  public get bcpAddress(): Address {
-    return this.signer.identityToAddress(this.bcpIdentity);
   }
 
   private readonly signer: MultiChainSigner;
   private readonly bnsConnection: AtomicSwapConnection;
-  private readonly bcpConnection: AtomicSwapConnection;
   // tslint:disable-next-line:readonly-keyword
   private preimage: Preimage | undefined;
 
   constructor(data: ActorData) {
     this.signer = data.signer;
     this.bnsConnection = data.bnsConnection;
-    this.bcpConnection = data.bcpConnection;
     this.bnsIdentity = data.bnsIdentity;
-    this.bcpIdentity = data.bcpIdentity;
   }
 
   // CASH is a token on BNS
@@ -110,21 +91,16 @@ class Actor {
     return new BN(amount ? amount.quantity : 0);
   }
 
-  // MASH is a token on BCP
-  public async getMashBalance(): Promise<BN> {
-    const account = await this.bcpConnection.getAccount({ pubkey: this.bcpIdentity.pubkey });
+  // BASH is a token on BNS
+  public async getBashBalance(): Promise<BN> {
+    const account = await this.bnsConnection.getAccount({ pubkey: this.bnsIdentity.pubkey });
     const balance = account ? account.balance : [];
-    const amount = balance.find(row => row.tokenTicker === MASH);
+    const amount = balance.find(row => row.tokenTicker === BASH);
     return new BN(amount ? amount.quantity : 0);
   }
 
   public async getBnsSwap(id: SwapIdBytes): Promise<AtomicSwap> {
     const swaps = await this.bnsConnection.getSwaps({ swapid: id });
-    return swaps[swaps.length - 1];
-  }
-
-  public async getBcpSwap(id: SwapIdBytes): Promise<AtomicSwap> {
-    const swaps = await this.bcpConnection.getSwaps({ swapid: id });
     return swaps[swaps.length - 1];
   }
 
@@ -153,16 +129,6 @@ class Actor {
     return this.sendTransaction(transaction);
   }
 
-  public async sendBcpTokens(recipient: Address, amount: Amount): Promise<Uint8Array | undefined> {
-    const transaction = await this.bcpConnection.withDefaultFee<SendTransaction>({
-      kind: "bcp/send",
-      creator: this.bcpIdentity,
-      recipient: recipient,
-      amount: amount,
-    });
-    return this.sendTransaction(transaction);
-  }
-
   public async sendSwapOfferOnBns(recipient: Address, amount: Amount): Promise<Uint8Array | undefined> {
     const transaction = await this.bnsConnection.withDefaultFee<SwapOfferTransaction>({
       kind: "bcp/swap_offer",
@@ -176,14 +142,14 @@ class Actor {
     return this.sendTransaction(transaction);
   }
 
-  public async sendSwapCounterOnBcp(
+  public async sendSwapCounterOnBns(
     offer: AtomicSwap,
     recipient: Address,
     amount: Amount,
   ): Promise<Uint8Array | undefined> {
-    const transaction = await this.bcpConnection.withDefaultFee<SwapOfferTransaction>({
+    const transaction = await this.bnsConnection.withDefaultFee<SwapOfferTransaction>({
       kind: "bcp/swap_offer",
-      creator: this.bcpIdentity,
+      creator: this.bnsIdentity,
       amounts: [amount],
       recipient: recipient,
       timeout: createTimestampTimeout(100),
@@ -192,10 +158,10 @@ class Actor {
     return this.sendTransaction(transaction);
   }
 
-  public async claimFromKnownPreimageOnBcp(offer: AtomicSwap): Promise<Uint8Array | undefined> {
-    const transaction = await this.bcpConnection.withDefaultFee<SwapClaimTransaction>({
+  public async claimFromKnownPreimageOnBns(offer: AtomicSwap): Promise<Uint8Array | undefined> {
+    const transaction = await this.bnsConnection.withDefaultFee<SwapClaimTransaction>({
       kind: "bcp/swap_claim",
-      creator: this.bcpIdentity,
+      creator: this.bnsIdentity,
       swapId: offer.data.id,
       preimage: this.preimage!,
     });
@@ -216,47 +182,42 @@ class Actor {
   }
 }
 
-describe("Full atomic swap between bns and bcp", () => {
+describe("Full atomic swap between BNS and BNS", () => {
   // TODO: handle different fees... right now with assumes 0.01 of the main token as fee
   it("works", async () => {
     pendingWithoutBnsd();
-    pendingWithoutBcpd();
 
     const alice = await Actor.create(
       "degree tackle suggest window test behind mesh extra cover prepare oak script",
       HdPaths.iov(0),
     );
     expect(alice.bnsAddress).toEqual("tiov15nuhg3l8ma2mdmcdvgy7hme20v3xy5mkxcezea");
-    expect(alice.bcpAddress).toEqual("tiov15nuhg3l8ma2mdmcdvgy7hme20v3xy5mkxcezea");
 
     const bob = await Actor.create(
       "dad kiss slogan offer outer bomb usual dream awkward jeans enlist mansion",
       HdPaths.iov(0),
     );
     expect(bob.bnsAddress).toEqual("tiov1qrw95py2x7fzjw25euuqlj6dq6t0jahe7rh8wp");
-    expect(bob.bcpAddress).toEqual("tiov1qrw95py2x7fzjw25euuqlj6dq6t0jahe7rh8wp");
 
-    // We need to send a 0.01 tokens to the other ones to allow claim fees
+    // TODO: let independent faucet send those tokens
+    // We need to send some native fee tokens to Bob
+    // 0.01 CASH send swap counter
+    // 0.01 CASH claim
     await alice.sendBnsTokens(bob.bnsAddress, {
-      quantity: "10000000",
+      quantity: "20000000",
       fractionalDigits: 9,
       tokenTicker: CASH,
     });
-    await bob.sendBcpTokens(alice.bcpAddress, {
-      quantity: "10000000",
-      fractionalDigits: 9,
-      tokenTicker: MASH,
-    });
 
-    // alice owns CASH on BNS but no MASH
+    // alice owns a lot of CASH on BNS
     const aliceInitialCash = await alice.getCashBalance();
-    const aliceInitialMash = await alice.getMashBalance();
-    expect(aliceInitialCash.gtn(100_000000000)).toEqual(true);
+    const aliceInitialBash = await alice.getBashBalance();
+    expect(aliceInitialCash.gt(new BN(100_000000000))).toEqual(true);
 
-    // bob owns MASH on BCP but no CASH
+    // bob owns a lot of BASH and some CASH (for fees) on BNS
     const bobInitialCash = await bob.getCashBalance();
-    const bobInitialMash = await bob.getMashBalance();
-    expect(bobInitialMash.gtn(100_000000000)).toEqual(true);
+    const bobInitialBash = await bob.getBashBalance();
+    expect(bobInitialBash.gt(new BN(100_000000000))).toEqual(true);
 
     // A secret that only Alice knows
     await alice.generatePreimage();
@@ -281,32 +242,32 @@ describe("Full atomic swap between bns and bcp", () => {
       tokenTicker: CASH,
     });
 
-    const bobOfferId = await bob.sendSwapCounterOnBcp(aliceOffer, alice.bcpAddress, {
+    const bobOfferId = await bob.sendSwapCounterOnBns(aliceOffer, alice.bnsAddress, {
       quantity: "5000000000",
       fractionalDigits: 9,
-      tokenTicker: MASH,
+      tokenTicker: BASH,
     });
 
-    // Bob's 5 MASH are locked in the contract (plus the fee deduction)
-    expect(bobInitialMash.sub(await bob.getMashBalance()).toString()).toEqual("5010000000");
+    // Bob's 5 BASH are locked in the contract
+    expect(bobInitialBash.sub(await bob.getBashBalance()).toString()).toEqual("5000000000");
 
-    // check correct counteroffer was made on BCP
-    const bobOffer = await alice.getBcpSwap(bobOfferId as SwapIdBytes);
+    // check correct counteroffer was made
+    const bobOffer = await alice.getBnsSwap(bobOfferId as SwapIdBytes);
     expect(bobOffer.kind).toEqual(SwapProcessState.Open);
-    expect(bobOffer.data.recipient).toEqual(alice.bcpAddress);
+    expect(bobOffer.data.recipient).toEqual(alice.bnsAddress);
     expect(bobOffer.data.amounts.length).toEqual(1);
     expect(bobOffer.data.amounts[0]).toEqual({
       quantity: "5000000000",
       fractionalDigits: 9,
-      tokenTicker: MASH,
+      tokenTicker: BASH,
     });
-    await alice.claimFromKnownPreimageOnBcp(bobOffer);
+    await alice.claimFromKnownPreimageOnBns(bobOffer);
 
-    // Alice revealed her secret and should own 5 MASH now
-    expect((await alice.getMashBalance()).sub(aliceInitialMash).toString()).toEqual("4990000000");
+    // Alice revealed her secret and should own 5 BASH now
+    expect((await alice.getBashBalance()).sub(aliceInitialBash).toString()).toEqual("5000000000");
 
-    // check claim was made on BCP
-    const aliceClaim = await bob.getBcpSwap(bobOfferId as SwapIdBytes);
+    // check claim was made
+    const aliceClaim = await bob.getBnsSwap(bobOfferId as SwapIdBytes);
     expect(aliceClaim.kind).toEqual(SwapProcessState.Claimed);
 
     await bob.claimFromRevealedPreimageOnBns(aliceClaim, aliceOfferId!);
@@ -315,13 +276,13 @@ describe("Full atomic swap between bns and bcp", () => {
     const bobClaim = await alice.getBnsSwap(aliceOfferId as SwapIdBytes);
     expect(bobClaim.kind).toEqual(SwapProcessState.Claimed);
 
-    // Bob used Alice's preimage to claim his 2 CASH
-    expect((await bob.getCashBalance()).sub(bobInitialCash).toString()).toEqual("1990000000");
+    // Bob used Alice's preimage to claim his 2 CASH (minus fees)
+    expect((await bob.getCashBalance()).sub(bobInitialCash).toString()).toEqual("1980000000");
 
     // Alice's CASH balance now down by 2 (plus fees)
-    expect(aliceInitialCash.sub(await alice.getCashBalance()).toString()).toEqual("2010000000");
+    expect(aliceInitialCash.sub(await alice.getCashBalance()).toString()).toEqual("2020000000");
 
-    // Bob's MASH balance now down by 5 (plus fees)
-    expect(bobInitialMash.sub(await bob.getMashBalance()).toString()).toEqual("5010000000");
+    // Bob's BASH balance now down by 5
+    expect(bobInitialBash.sub(await bob.getBashBalance()).toString()).toEqual("5000000000");
   });
 });
--- scripts/bnsd/README.md
+++ scripts/bnsd/README.md
@@ -41,3 +41,14 @@ path: m/1229936198'/1'/0'/0'
 pubkey: e05f47e7639b47625c23738e2e46d092819abd6039c5fc550d9aa37f1a2556a1
 IOV address: tiov1q5lyl7asgr2dcweqrhlfyexqpkgcuzrm4e0cku
 ```
+
+### The Bob account in atomic swap tests with BASH balance
+
+```
+mnemonic: dad kiss slogan offer outer bomb usual dream awkward jeans enlist mansion
+
+path: m/44'/234'/0'
+pubkey: 1e93dfd6b1d897fb5037f389e8ebfb14dc08b50c151b0a335ac9eeeb56f8b752
+IOV address: tiov1qrw95py2x7fzjw25euuqlj6dq6t0jahe7rh8wp
+hex address: 00dc5a048a3792293954cf380fcb4d0696f976f9
+```
