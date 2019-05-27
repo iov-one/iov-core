d866c0da1f265fc573ad2b98e1a082941a885552
Simon Warta <simon@kullo.net>
2019-05-27 12:22:31 +0200

    Remove BCPD scripts

--- scripts/bcpd/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit -o nounset -o pipefail
-command -v shellcheck > /dev/null && shellcheck "$0"
-
-echo "Killing BCPd containers ..."
-docker container kill "bcpd-app" "bcpd-tendermint"
