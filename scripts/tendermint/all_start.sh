3fac865c61ece6f187cfbe6f4dabd8cf6bf6b4d7
Simon Warta <simon@kullo.net>
2019-05-28 09:35:21 +0200

    Upgrade Tendermint 29 to 0.29.2

--- scripts/tendermint/all_start.sh
+++ scripts/tendermint/all_start.sh
@@ -2,9 +2,10 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
+# Find latest patch releases at https://hub.docker.com/r/tendermint/tendermint/tags/
 declare -a TM_VERSIONS
 TM_VERSIONS[27]=0.27.4
-TM_VERSIONS[29]=0.29.1
+TM_VERSIONS[29]=0.29.2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
--- scripts/tendermint/all_stop.sh
+++ scripts/tendermint/all_stop.sh
@@ -4,7 +4,7 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 
 declare -a TM_VERSIONS
 TM_VERSIONS[27]=0.27.4
-TM_VERSIONS[29]=0.29.1
+TM_VERSIONS[29]=0.29.2
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
