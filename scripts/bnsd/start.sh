5d53d0620e0502bc35d31b3ec9b2e9a258728659
Simon Warta <simon@kullo.net>
2019-05-29 14:42:35 +0200

    Update local chain to v0.15.0/v0.31.5

--- scripts/bnsd/start.sh
+++ scripts/bnsd/start.sh
@@ -3,9 +3,9 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
 # Choose from https://hub.docker.com/r/iov1/tendermint/tags/
-export BNSD_TM_VERSION=v0.29.1
+export BNSD_TM_VERSION=v0.31.5
 # Choose from https://hub.docker.com/r/iov1/bnsd/tags/
-export BNSD_VERSION=v0.14.0
+export BNSD_VERSION=v0.15.0
 
 # get this files directory regardless of pwd when we run it
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
