f134da344900a3a6e24491cab5ad5871ec290c3b
Simon Warta <simon@kullo.net>
2019-06-03 17:38:09 +0200

    Update comments and docs

--- packages/iov-tendermint-rpc/src/config.spec.ts
+++ packages/iov-tendermint-rpc/src/config.spec.ts
@@ -10,7 +10,6 @@ export interface TendermintInstance {
  * Testing different versions: as a convention, the minor version number is encoded
  * in the port 111<version>, e.g. Tendermint 0.21.0 runs on port 11121. To start
  * a specific version use:
- *   TENDERMINT_VERSION=0.27.4 TENDERMINT_PORT=11127 ./scripts/tendermint/start.sh
  *   TENDERMINT_VERSION=0.29.2 TENDERMINT_PORT=11129 ./scripts/tendermint/start.sh
  *
  * When more than 1 instances of tendermint are running, stop them manually:
