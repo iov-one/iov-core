075a2a424d7831bdc5fa41820e560b3a0214d737
willclarktech <willclarktech@users.noreply.github.com>
2019-06-12 16:20:28 +0100

    Add stop script for socketserver

---
+++ scripts/socketserver/stop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pkill -f "$SCRIPT_DIR/echo.py --port 444[45]"
