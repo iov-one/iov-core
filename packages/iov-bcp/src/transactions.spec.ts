270cfecad8091292c77b58aff05fb79043f1c646
Simon Warta <simon@warta.it>
2019-07-23 08:44:10 +0200

    Add tests for isPubkeyBundle, isIdentity

--- packages/iov-bcp/src/transactions.spec.ts
+++ packages/iov-bcp/src/transactions.spec.ts
@@ -1,6 +1,80 @@
-import { isBlockHeightTimeout, isTimestampTimeout } from "./transactions";
+import { Encoding } from "@iov/encoding";
+
+import {
+  Algorithm,
+  ChainId,
+  Identity,
+  isBlockHeightTimeout,
+  isIdentity,
+  isPubkeyBundle,
+  isTimestampTimeout,
+  PubkeyBundle,
+  PubkeyBytes,
+} from "./transactions";
+
+const { fromHex } = Encoding;
 
 describe("transactions", () => {
+  describe("isPubkeyBundle", () => {
+    it("returns true for valid PubkeyBundle", () => {
+      const good: PubkeyBundle = {
+        algo: Algorithm.Ed25519,
+        data: fromHex("aabbccdd") as PubkeyBytes,
+      };
+      expect(isPubkeyBundle(good)).toEqual(true);
+    });
+
+    it("returns true for PubkeyBundle as deserialized from json", () => {
+      const good = {
+        algo: "ed25519",
+        data: fromHex("aabbccdd"),
+      };
+      expect(isPubkeyBundle(good)).toEqual(true);
+    });
+
+    it("returns false for a bunch of other stuff", () => {
+      expect(isPubkeyBundle("abc")).toEqual(false);
+      expect(isPubkeyBundle({})).toEqual(false);
+      expect(isPubkeyBundle(null)).toEqual(false);
+      expect(isPubkeyBundle(undefined)).toEqual(false);
+      expect(isPubkeyBundle(fromHex("aabb"))).toEqual(false);
+      // TODO: test with missing algo, data once we have TS3.5 omit type
+    });
+  });
+
+  describe("isIdentity", () => {
+    it("returns true for valid Identity", () => {
+      const good: Identity = {
+        chainId: "foobar" as ChainId,
+        pubkey: {
+          algo: Algorithm.Ed25519,
+          data: fromHex("aabbccdd") as PubkeyBytes,
+        },
+      };
+      expect(isIdentity(good)).toEqual(true);
+    });
+
+    it("returns true for Identity as deserialized from json", () => {
+      const good = {
+        chainId: "foobar",
+        pubkey: {
+          algo: "ed25519",
+          data: fromHex("aabbccdd"),
+        },
+      };
+      expect(isIdentity(good)).toEqual(true);
+    });
+
+    it("returns false for a bunch of other stuff", () => {
+      expect(isIdentity("abc")).toEqual(false);
+      expect(isIdentity({})).toEqual(false);
+      expect(isIdentity(null)).toEqual(false);
+      expect(isIdentity(undefined)).toEqual(false);
+      expect(isIdentity(fromHex("aabb"))).toEqual(false);
+      // TODO: test with missing chainId, pubkey once we have TS3.5 omit type
+    });
+  });
+
   describe("isBlockHeightTimeout", () => {
     it("returns true for block height timeouts", () => {
       expect(isBlockHeightTimeout({ height: 1 })).toEqual(true);
