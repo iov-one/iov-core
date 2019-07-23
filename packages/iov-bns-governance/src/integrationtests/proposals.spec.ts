19a8ea924929c8cd15df63a04f4035c195c9dc68
willclarktech <willclarktech@users.noreply.github.com>
2019-07-23 17:58:30 +0200

    Add bns-governance integration test: Add/RemoveCommitteeMember

--- packages/iov-bns-governance/src/integrationtests/proposals.spec.ts
+++ packages/iov-bns-governance/src/integrationtests/proposals.spec.ts
@@ -5,11 +5,19 @@ import {
   UnsignedTransaction,
   WithCreator,
 } from "@iov/bcp";
-import { ActionKind, bnsCodec, BnsConnection, ProposalStatus, VoteOption } from "@iov/bns";
+import {
+  ActionKind,
+  bnsCodec,
+  BnsConnection,
+  ProposalExecutorResult,
+  ProposalStatus,
+  VoteOption,
+} from "@iov/bns";
 import { Encoding } from "@iov/encoding";
 import { Ed25519HdWallet, HdPaths, UserProfile } from "@iov/keycontrol";
 import { ReadonlyDate } from "readonly-date";
 
+import { CommitteeId } from "../committees";
 import { Governor, GovernorOptions } from "../governor";
 import { ProposalOptions, ProposalType } from "../proposals";
 
@@ -34,13 +42,14 @@ async function sleep(ms: number): Promise<void> {
 
 async function getGovernorOptions(
   path = faucetPath,
-): Promise<GovernorOptions & { readonly profile: UserProfile }> {
+): Promise<GovernorOptions & { readonly profile: UserProfile; readonly address: Address }> {
   const connection = await BnsConnection.establish(bnsdUrl);
   const chainId = await connection.chainId();
   const profile = new UserProfile();
   const wallet = profile.addWallet(Ed25519HdWallet.fromMnemonic(faucetMnemonic));
   const identity = await profile.createIdentity(wallet.id, chainId, path);
   return {
+    address: bnsCodec.identityToAddress(identity),
     connection: connection,
     identity: identity,
     guaranteeFundEscrowId: guaranteeFundEscrowId,
@@ -65,7 +74,131 @@ async function signAndPost(
 }
 
 describe("Proposals", () => {
-  it("works for adding and removing committee members");
+  it("works for adding and removing committee members", async () => {
+    pendingWithoutBnsd();
+    const governorOptions = await getGovernorOptions();
+    const { address, connection, profile } = governorOptions;
+    const governor = new Governor(governorOptions);
+    const numProposalsBefore = (await governor.getProposals()).length;
+
+    const committeeId = 2 as CommitteeId;
+    const electionRuleId = 2;
+    const newCommitteeMember = "tiov12shyht3pvvacvyee36w5844jkfh5s0mf4gszp9" as Address;
+
+    // Add committee member
+
+    const startTime1 = new ReadonlyDate(Date.now() + 1_000);
+    const proposal1Options: ProposalOptions = {
+      type: ProposalType.AddCommitteeMember,
+      title: "Add committee member",
+      description: "Add a committee member in more detail",
+      startTime: startTime1,
+      electionRuleId: electionRuleId,
+      committee: committeeId,
+      address: newCommitteeMember,
+      weight: 3,
+    };
+    const createProposal1Tx = await governor.buildCreateProposalTx(proposal1Options);
+    await signAndPost(createProposal1Tx, connection, profile);
+
+    const proposalsAfterCreate1 = await governor.getProposals();
+    expect(proposalsAfterCreate1.length).toEqual(numProposalsBefore + 1);
+    const proposal1Pre = proposalsAfterCreate1[proposalsAfterCreate1.length - 1];
+    expect(proposal1Pre.title).toEqual("Add committee member");
+    expect(proposal1Pre.description).toEqual("Add a committee member in more detail");
+    expect(new ReadonlyDate(proposal1Pre.votingStartTime * 1000).toDateString()).toEqual(
+      startTime1.toDateString(),
+    );
+    expect(proposal1Pre.electionRule.id).toEqual(electionRuleId);
+    expect(proposal1Pre.author).toEqual(address);
+    expect(proposal1Pre.action).toEqual({
+      kind: ActionKind.UpdateElectorate,
+      electorateId: committeeId,
+      diffElectors: {
+        [newCommitteeMember]: { weight: 3 },
+      },
+    });
+    expect(proposal1Pre.status).toEqual(ProposalStatus.Submitted);
+
+    await sleep(4000);
+
+    const vote1Tx = await governor.buildVoteTx(proposal1Pre.id, VoteOption.Yes);
+    await signAndPost(vote1Tx, connection, profile);
+
+    await sleep(15000);
+
+    const proposalsAfterVote1 = await governor.getProposals();
+    expect(proposalsAfterVote1.length).toEqual(numProposalsBefore + 1);
+    const proposal1Post = proposalsAfterVote1[proposalsAfterVote1.length - 1];
+    expect(proposal1Post.id).toEqual(proposal1Pre.id);
+    expect(proposal1Post.title).toEqual("Add committee member");
+    expect(proposal1Post.status).toEqual(ProposalStatus.Closed);
+    expect(proposal1Post.executorResult).toEqual(ProposalExecutorResult.Succeeded);
+
+    const electorates1 = (await governor.getElectorates()).filter(({ id }) => id === committeeId);
+    const electorateWithAddedMember = electorates1[electorates1.length - 1];
+    expect(electorateWithAddedMember.electors).toEqual({
+      [address]: { weight: 10 },
+      [newCommitteeMember]: { weight: 3 },
+    });
+
+    // Remove committee member
+
+    const startTime2 = new ReadonlyDate(Date.now() + 1_000);
+    const proposal2Options: ProposalOptions = {
+      type: ProposalType.RemoveCommitteeMember,
+      title: "Remove committee member",
+      description: "Remove a committee member in more detail",
+      startTime: startTime2,
+      electionRuleId: electionRuleId,
+      committee: committeeId,
+      address: newCommitteeMember,
+    };
+    const createProposal2Tx = await governor.buildCreateProposalTx(proposal2Options);
+    await signAndPost(createProposal2Tx, connection, profile);
+
+    const proposalsAfterCreate2 = await governor.getProposals();
+    expect(proposalsAfterCreate2.length).toEqual(numProposalsBefore + 2);
+    const proposal2Pre = proposalsAfterCreate2[proposalsAfterCreate2.length - 1];
+    expect(proposal2Pre.title).toEqual("Remove committee member");
+    expect(proposal2Pre.description).toEqual("Remove a committee member in more detail");
+    expect(new ReadonlyDate(proposal2Pre.votingStartTime * 1000).toDateString()).toEqual(
+      startTime2.toDateString(),
+    );
+    expect(proposal2Pre.electionRule.id).toEqual(electionRuleId);
+    expect(proposal2Pre.author).toEqual(address);
+    expect(proposal2Pre.action).toEqual({
+      kind: ActionKind.UpdateElectorate,
+      electorateId: committeeId,
+      diffElectors: {
+        [newCommitteeMember]: { weight: 0 },
+      },
+    });
+    expect(proposal2Pre.status).toEqual(ProposalStatus.Submitted);
+
+    await sleep(4000);
+
+    const vote2Tx = await governor.buildVoteTx(proposal2Pre.id, VoteOption.Yes);
+    await signAndPost(vote2Tx, connection, profile);
+
+    await sleep(15000);
+
+    const proposalsAfterVote2 = await governor.getProposals();
+    expect(proposalsAfterVote2.length).toEqual(numProposalsBefore + 2);
+    const proposal2Post = proposalsAfterVote2[proposalsAfterVote2.length - 1];
+    expect(proposal2Post.id).toEqual(proposal2Pre.id);
+    expect(proposal2Post.title).toEqual("Remove committee member");
+    expect(proposal2Post.status).toEqual(ProposalStatus.Closed);
+    expect(proposal2Post.executorResult).toEqual(ProposalExecutorResult.Succeeded);
+
+    const electorates2 = (await governor.getElectorates()).filter(({ id }) => id === committeeId);
+    const electorateWithRemovedMember = electorates2[electorates2.length - 1];
+    expect(electorateWithRemovedMember.electors).toEqual({
+      [address]: { weight: 10 },
+    });
+
+    connection.disconnect();
+  }, 60_000);
 
   it("works for amending election rule threshold/quorum");
 
@@ -76,7 +209,7 @@ describe("Proposals", () => {
   it("works for amending the protocol", async () => {
     pendingWithoutBnsd();
     const governorOptions = await getGovernorOptions();
-    const { connection, identity, profile } = governorOptions;
+    const { address, connection, profile } = governorOptions;
     const governor = new Governor(governorOptions);
     const numProposalsBefore = (await governor.getProposals()).length;
 
@@ -102,7 +235,7 @@ describe("Proposals", () => {
       startTime.toDateString(),
     );
     expect(proposal1.electionRule.id).toEqual(electionRuleId);
-    expect(proposal1.author).toEqual(bnsCodec.identityToAddress(identity));
+    expect(proposal1.author).toEqual(address);
     expect(proposal1.action).toEqual({
       kind: ActionKind.CreateTextResolution,
       resolution: "Give IOV devs master keys to all accounts",
