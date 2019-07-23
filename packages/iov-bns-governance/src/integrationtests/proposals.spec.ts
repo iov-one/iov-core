983fb3c0793c5fb8e2f075c730f94c6c3b056274
willclarktech <willclarktech@users.noreply.github.com>
2019-07-23 14:58:10 +0200

    Add skeleton suite for bns-governance integration tests

---
+++ packages/iov-bns-governance/src/integrationtests/proposals.spec.ts
@@ -0,0 +1,11 @@
+describe("Proposals", () => {
+  it("works for adding and removing committee members");
+
+  it("works for amending election rule threshold/quorum");
+
+  it("works for adding and removing validators");
+
+  it("works for releasing and distributing guarantee funds");
+
+  it("works for amending the protocol");
+});
