9801228491cbb04f8da71f01abee580ba3d713ee
willclarktech <willclarktech@users.noreply.github.com>
2019-07-25 12:31:56 +0200

    Add bns-governance integration test: Add/RemoveValidator

--- packages/iov-bns-governance/src/integrationtests/proposals.spec.ts
+++ packages/iov-bns-governance/src/integrationtests/proposals.spec.ts
@@ -338,7 +338,127 @@ describe("Proposals", () => {
     connection.disconnect();
   }, 60_000);
 
-  it("works for adding and removing validators");
+  it("works for adding and removing validators", async () => {
+    pendingWithoutBnsd();
+    const governorOptions = await getGovernorOptions();
+    const { address, connection, identity, profile } = governorOptions;
+    const governor = new Governor(governorOptions);
+    const numProposalsBefore = (await governor.getProposals()).length;
+    const numValidatorsBefore = (await connection.getValidators()).length;
+    const electionRuleId = 2;
+
+    // Add validator
+
+    const startTime1 = new ReadonlyDate(Date.now() + 1_000);
+    const proposal1Options: ProposalOptions = {
+      type: ProposalType.AddValidator,
+      title: "Add validator",
+      description: "Add a validator in more detail",
+      startTime: startTime1,
+      electionRuleId: electionRuleId,
+      pubkey: identity.pubkey,
+      power: 2,
+    };
+    const createProposal1Tx = await governor.buildCreateProposalTx(proposal1Options);
+    await signAndPost(createProposal1Tx, connection, profile);
+
+    const proposalsAfterCreate1 = await governor.getProposals();
+    expect(proposalsAfterCreate1.length).toEqual(numProposalsBefore + 1);
+    const proposal1Pre = proposalsAfterCreate1[proposalsAfterCreate1.length - 1];
+    expect(proposal1Pre.title).toEqual("Add validator");
+    expect(proposal1Pre.description).toEqual("Add a validator in more detail");
+    expect(new ReadonlyDate(proposal1Pre.votingStartTime * 1000).toDateString()).toEqual(
+      startTime1.toDateString(),
+    );
+    expect(proposal1Pre.electionRule.id).toEqual(electionRuleId);
+    expect(proposal1Pre.author).toEqual(address);
+    expect(proposal1Pre.action).toEqual({
+      kind: ActionKind.SetValidators,
+      validatorUpdates: {
+        [`ed25519_${Encoding.toHex(identity.pubkey.data)}`]: { power: 2 },
+      },
+    });
+    expect(proposal1Pre.status).toEqual(ProposalStatus.Submitted);
+
+    await sleep(3000);
+
+    const vote1Tx = await governor.buildVoteTx(proposal1Pre.id, VoteOption.Yes);
+    await signAndPost(vote1Tx, connection, profile);
+
+    await sleep(15000);
+
+    const proposalsAfterVote1 = await governor.getProposals();
+    expect(proposalsAfterVote1.length).toEqual(numProposalsBefore + 1);
+    const proposal1Post = proposalsAfterVote1[proposalsAfterVote1.length - 1];
+    expect(proposal1Post.id).toEqual(proposal1Pre.id);
+    expect(proposal1Post.title).toEqual("Add validator");
+    expect(proposal1Post.status).toEqual(ProposalStatus.Closed);
+    expect(proposal1Post.executorResult).toEqual(ProposalExecutorResult.Succeeded);
+
+    const validatorsAfter1 = await connection.getValidators();
+    expect(validatorsAfter1.length).toEqual(numValidatorsBefore + 1);
+    const addedValidator = validatorsAfter1.find(
+      validator => Encoding.toHex(validator.pubkey.data) === Encoding.toHex(identity.pubkey.data),
+    );
+    expect(addedValidator).toBeDefined();
+    expect(addedValidator!.power).toEqual(2);
+
+    // Remove validator
+
+    const startTime2 = new ReadonlyDate(Date.now() + 1_000);
+    const proposal2Options: ProposalOptions = {
+      type: ProposalType.RemoveValidator,
+      title: "Remove validator",
+      description: "Remove a validator in more detail",
+      startTime: startTime2,
+      electionRuleId: electionRuleId,
+      pubkey: identity.pubkey,
+    };
+    const createProposal2Tx = await governor.buildCreateProposalTx(proposal2Options);
+    await signAndPost(createProposal2Tx, connection, profile);
+
+    const proposalsAfterCreate2 = await governor.getProposals();
+    expect(proposalsAfterCreate2.length).toEqual(numProposalsBefore + 2);
+    const proposal2Pre = proposalsAfterCreate2[proposalsAfterCreate2.length - 1];
+    expect(proposal2Pre.title).toEqual("Remove validator");
+    expect(proposal2Pre.description).toEqual("Remove a validator in more detail");
+    expect(new ReadonlyDate(proposal2Pre.votingStartTime * 1000).toDateString()).toEqual(
+      startTime2.toDateString(),
+    );
+    expect(proposal2Pre.electionRule.id).toEqual(electionRuleId);
+    expect(proposal2Pre.author).toEqual(address);
+    expect(proposal2Pre.action).toEqual({
+      kind: ActionKind.SetValidators,
+      validatorUpdates: {
+        [`ed25519_${Encoding.toHex(identity.pubkey.data)}`]: { power: 0 },
+      },
+    });
+    expect(proposal2Pre.status).toEqual(ProposalStatus.Submitted);
+
+    await sleep(3000);
+
+    const vote2Tx = await governor.buildVoteTx(proposal2Pre.id, VoteOption.Yes);
+    await signAndPost(vote2Tx, connection, profile);
+
+    await sleep(15000);
+
+    const proposalsAfterVote2 = await governor.getProposals();
+    expect(proposalsAfterVote2.length).toEqual(numProposalsBefore + 2);
+    const proposal2Post = proposalsAfterVote2[proposalsAfterVote2.length - 1];
+    expect(proposal2Post.id).toEqual(proposal2Pre.id);
+    expect(proposal2Post.title).toEqual("Remove validator");
+    expect(proposal2Post.status).toEqual(ProposalStatus.Closed);
+    expect(proposal2Post.executorResult).toEqual(ProposalExecutorResult.Succeeded);
+
+    const validatorsAfter2 = await connection.getValidators();
+    expect(validatorsAfter2.length).toEqual(numValidatorsBefore);
+    const removedValidator = validatorsAfter2.find(
+      validator => Encoding.toHex(validator.pubkey.data) === Encoding.toHex(identity.pubkey.data),
+    );
+    expect(removedValidator).not.toBeDefined();
+
+    connection.disconnect();
+  }, 60_000);
 
   it("works for releasing and distributing guarantee funds");
 
