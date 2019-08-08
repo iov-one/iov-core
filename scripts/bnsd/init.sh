4b6cf7ab04a40dba4c3842864e5e248f379f8305
willclarktech <willclarktech@users.noreply.github.com>
2019-08-08 10:48:37 +0200

    Use SCRIPT_DIR in bnsd init script

--- scripts/bnsd/init.sh
+++ scripts/bnsd/init.sh
@@ -2,9 +2,12 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
+# get this files directory regardless of pwd when we run it
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ -n "${INIT_PROPOSALS:-}" ]; then
   sleep 3
   echo "Deploying proposals..."
-  node "./deploy_proposals.js"
+  node "${SCRIPT_DIR}/deploy_proposals.js"
   echo "Done."
 fi
