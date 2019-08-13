89c496dfa240a5a8d8cfaa5f25bf7c7676b9b347
willclarktech <willclarktech@users.noreply.github.com>
2019-08-13 13:22:19 +0200

    Update ChainConnector type in BCP

--- packages/iov-bcp/types/chainconnector.d.ts
+++ packages/iov-bcp/types/chainconnector.d.ts
@@ -2,7 +2,7 @@ import { TxCodec } from "./codec";
 import { BlockchainConnection } from "./connection";
 import { ChainId } from "./transactions";
 export interface ChainConnector {
-  readonly client: () => Promise<BlockchainConnection>;
+  readonly establishConnection: () => Promise<BlockchainConnection>;
   readonly codec: TxCodec;
-  readonly expectedChainId?: ChainId;
+  readonly expectedChainId: ChainId | undefined;
 }
