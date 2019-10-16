5dfc6099043c574025ee45c4cb010436045d41a6
willclarktech <willclarktech@users.noreply.github.com>
2019-10-16 11:03:59 +0100

    Add cosmos codec transaction parsing test

--- packages/iov-cosmos/src/cosmoscodec.spec.ts
+++ packages/iov-cosmos/src/cosmoscodec.spec.ts
@@ -1,9 +1,16 @@
+import { PostableBytes } from "@iov/bcp";
+
 import { cosmosCodec } from "./cosmoscodec";
-import { signedTxBin, signedTxJson } from "./testdata.spec";
+import { chainId, signedTxBin, signedTxJson } from "./testdata.spec";
 
 describe("cosmoscodec", () => {
   it("properly encodes transactions", () => {
     const encoded = cosmosCodec.bytesToPost(signedTxJson);
     expect(encoded).toEqual(signedTxBin);
+  });
+
+  it("properly decodes transactions", () => {
+    const decoded = cosmosCodec.parseBytes(signedTxBin as PostableBytes, chainId);
+    expect(decoded).toEqual(signedTxJson);
   });
 });
