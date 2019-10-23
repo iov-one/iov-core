6f673e7da2d601a453bc88568004ee636c205430
willclarktech <willclarktech@users.noreply.github.com>
2019-10-23 18:44:02 +0200

    Add basic post/get tx methods to Cosmos REST client

--- packages/iov-cosmos/types/restclient.d.ts
+++ packages/iov-cosmos/types/restclient.d.ts
@@ -1,4 +1,4 @@
-import { Address } from "@iov/bcp";
+import { Address, PostableBytes, TransactionId } from "@iov/bcp";
 import amino from "@tendermint/amino-js";
 interface NodeInfo {
   readonly network: string;
@@ -30,14 +30,33 @@ interface AuthAccountsResponse {
     readonly value: amino.BaseAccount;
   };
 }
-declare type RestClientResponse = NodeInfoResponse | BlocksResponse | AuthAccountsResponse;
+interface TxsResponse {
+  readonly height: string;
+  readonly txhash: string;
+  readonly raw_log: string;
+  readonly tx: amino.Tx;
+}
+interface PostTxsParams {}
+interface PostTxsResponse {
+  readonly height: string;
+  readonly txhash: string;
+  readonly code?: number;
+  readonly raw_log?: string;
+}
+declare type RestClientResponse = NodeInfoResponse | BlocksResponse | AuthAccountsResponse | PostTxsResponse;
+declare type BroadcastMode = "block" | "sync" | "async";
 export declare class RestClient {
   private readonly baseUrl;
-  constructor(url: string);
+  private readonly postConfig;
+  private readonly mode;
+  constructor(url: string, mode?: BroadcastMode);
   get(path: string): Promise<RestClientResponse>;
+  post(path: string, params: PostTxsParams): Promise<RestClientResponse>;
   nodeInfo(): Promise<NodeInfoResponse>;
   blocksLatest(): Promise<BlocksResponse>;
   blocks(height: number): Promise<BlocksResponse>;
   authAccounts(address: Address): Promise<AuthAccountsResponse>;
+  txsById(id: TransactionId): Promise<TxsResponse>;
+  postTx(tx: PostableBytes): Promise<PostTxsResponse>;
 }
 export {};
