6775c28c55ec574d47d3a04b92afc79b0459e735
willclarktech <willclarktech@users.noreply.github.com>
2019-10-29 10:36:57 +0100

    Accept a nonce in Cosmos codec parseBytes function

--- packages/iov-cosmos/types/cosmoscodec.d.ts
+++ packages/iov-cosmos/types/cosmoscodec.d.ts
@@ -14,7 +14,7 @@ export declare class CosmosCodec implements TxCodec {
   bytesToSign(unsigned: UnsignedTransaction, nonce: Nonce): SigningJob;
   bytesToPost(signed: SignedTransaction): PostableBytes;
   identifier(signed: SignedTransaction): TransactionId;
-  parseBytes(bytes: PostableBytes, chainId: ChainId): SignedTransaction;
+  parseBytes(bytes: PostableBytes, chainId: ChainId, nonce?: Nonce): SignedTransaction;
   identityToAddress(identity: Identity): Address;
   isValidAddress(address: string): boolean;
 }
