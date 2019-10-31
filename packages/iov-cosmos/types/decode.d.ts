d416e382b068bd80736388b4bb00cec735afa1fe
willclarktech <willclarktech@users.noreply.github.com>
2019-10-31 15:02:31 +0100

    Create custom AminoTx type for now

--- packages/iov-cosmos/types/decode.d.ts
+++ packages/iov-cosmos/types/decode.d.ts
@@ -11,6 +11,7 @@ import {
   SignedTransaction,
 } from "@iov/bcp";
 import amino from "@tendermint/amino-js";
+import { AminoTx } from "./types";
 export declare function decodePubkey(pubkey: amino.PubKey): PubkeyBundle;
 export declare function decodeSignature(signature: string): SignatureBytes;
 export declare function decodeFullSignature(signature: amino.StdSignature, nonce: number): FullSignature;
@@ -18,4 +19,4 @@ export declare function decodeAmount(amount: amino.Coin): Amount;
 export declare function parseMsg(msgs: readonly amino.Msg[]): SendTransaction;
 export declare function parseFee(fee: amino.StdFee): Fee;
 export declare function parseCreator(signature: amino.StdSignature, chainId: ChainId): Identity;
-export declare function parseTx(tx: amino.Tx, chainId: ChainId, nonce: Nonce): SignedTransaction;
+export declare function parseTx(tx: AminoTx, chainId: ChainId, nonce: Nonce): SignedTransaction;
