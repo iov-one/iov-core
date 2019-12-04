e6b2b7980d8f6d77188470f503d8424b10655a50
willclarktech <willclarktech@users.noreply.github.com>
2019-12-04 16:59:37 +0100

    Disable tslint no-empty rule
    This is now covered by @typescript-eslint/no-empty-function

--- packages/iov-stream/src/promise.ts
+++ packages/iov-stream/src/promise.ts
@@ -16,7 +16,6 @@ export function fromListPromise<T>(promise: Promise<Iterable<T>>): Stream<T> {
         })
         .catch(error => listener.error(error));
     },
-    // tslint:disable:no-empty
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     stop: () => {},
   };
