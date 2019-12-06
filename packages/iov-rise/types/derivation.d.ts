ce3f2d4c556baca99a909ca2f689f6f10811bdcb
Simon Warta <simon@warta.it>
2019-12-06 10:49:05 +0100

    Remove @iov/rise

--- packages/iov-rise/types/derivation.d.ts
+++
@@ -1,3 +0,0 @@
-import { Address } from "@iov/bcp";
-export declare function pubkeyToAddress(pubkey: Uint8Array): Address;
-export declare function isValidAddress(address: string): boolean;
--- packages/iov-rise/types/riseconnector.d.ts
+++
@@ -1,6 +0,0 @@
-import { ChainConnector, ChainId } from "@iov/bcp";
-import { RiseConnection } from "./riseconnection";
-export declare function createRiseConnector(
-  url: string,
-  expectedChainId?: ChainId,
-): ChainConnector<RiseConnection>;
