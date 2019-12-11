8821cf68e5f7ae2372bd52a70265c6ac64b62f24
willclarktech <willclarktech@users.noreply.github.com>
2019-12-11 11:35:34 +0100

    Remove WithChainId from cli

--- packages/iov-cli/src/cli.ts
+++ packages/iov-cli/src/cli.ts
@@ -44,7 +44,6 @@ export function main(originalArgs: readonly string[]): void {
         "SendTransaction",
         "TokenTicker",
         "TransactionId",
-        "WithChainId",
         // block info
         "BlockInfoPending",
         "BlockInfoSucceeded",
