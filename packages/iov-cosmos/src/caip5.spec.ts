d6cbad55f01cf86b169d99ca9a2170402d81f04a
Simon Warta <simon@warta.it>
2020-01-16 12:50:53 +0100

    Update Caip5 implementation to latest version of spec

--- packages/iov-cosmos/src/caip5.spec.ts
+++ packages/iov-cosmos/src/caip5.spec.ts
@@ -4,28 +4,31 @@ import { Caip5 } from "./caip5";
 
 describe("Caip5", () => {
   describe("encode", () => {
-    it("works for valid format", () => {
+    it("works for direct format", () => {
       expect(Caip5.encode("foo")).toEqual("cosmos:foo");
       expect(Caip5.encode("aA1-")).toEqual("cosmos:aA1-");
       expect(Caip5.encode("12345678901234567890123456789012345678901234567")).toEqual(
         "cosmos:12345678901234567890123456789012345678901234567",
       );
+
+      // Test vectors from CAIP-5
+      expect(Caip5.encode("cosmoshub-3")).toEqual("cosmos:cosmoshub-3");
+      expect(Caip5.encode("Binance-Chain-Tigris")).toEqual("cosmos:Binance-Chain-Tigris");
+      expect(Caip5.encode("hash-")).toEqual("cosmos:hash-");
+      expect(Caip5.encode("hashed")).toEqual("cosmos:hashed");
     });
 
-    it("throws for invalid format", () => {
-      // too short
-      expect(() => Caip5.encode("")).toThrowError(/^Given chain ID cannot be CAIP-5 encoded/i);
-      expect(() => Caip5.encode("1")).toThrowError(/^Given chain ID cannot be CAIP-5 encoded/i);
-      expect(() => Caip5.encode("12")).toThrowError(/^Given chain ID cannot be CAIP-5 encoded/i);
-
-      // too long
-      expect(() => Caip5.encode("123456789012345678901234567890123456789012345678")).toThrowError(
-        /^Given chain ID cannot be CAIP-5 encoded/i,
+    it("works for hashed format", () => {
+      // Test vectors from CAIP-5
+      expect(Caip5.encode("")).toEqual("cosmos:hashed-e3b0c44298fc1c14");
+      expect(Caip5.encode("x")).toEqual("cosmos:hashed-2d711642b726b044");
+      expect(Caip5.encode("hashed-")).toEqual("cosmos:hashed-c904589232422def");
+      expect(Caip5.encode("hashed-123")).toEqual("cosmos:hashed-99df5cd68192b33e");
+      expect(Caip5.encode("123456789012345678901234567890123456789012345678")).toEqual(
+        "cosmos:hashed-0204c92a0388779d",
       );
-
-      // invalid chars
-      expect(() => Caip5.encode("foo bar")).toThrowError(/^Given chain ID cannot be CAIP-5 encoded/i);
-      expect(() => Caip5.encode("wonderland🧝‍♂️")).toThrowError(/^Given chain ID cannot be CAIP-5 encoded/i);
+      expect(Caip5.encode(" ")).toEqual("cosmos:hashed-36a9e7f1c95b82ff");
+      expect(Caip5.encode("wonderland🧝‍♂️")).toEqual("cosmos:hashed-843d2fc87f40eeb9");
     });
   });
 
@@ -39,8 +42,9 @@ describe("Caip5", () => {
     });
 
     it("throws for invalid format", () => {
-      // wrong interface
+      // wrong namespace
       expect(() => Caip5.decode(":foobar" as ChainId)).toThrowError(/not compatible with CAIP-5/i);
+      expect(() => Caip5.decode("xyz:foobar" as ChainId)).toThrowError(/not compatible with CAIP-5/i);
       expect(() => Caip5.decode("cosmos-hash:foobar" as ChainId)).toThrowError(/not compatible with CAIP-5/i);
 
       // too short
@@ -55,8 +59,18 @@ describe("Caip5", () => {
 
       // invalid chars
       expect(() => Caip5.decode("cosmos:foo bar" as ChainId)).toThrowError(/not compatible with CAIP-5/i);
-      expect(() => Caip5.decode("cosmos:wonderland🧝‍♂️" as ChainId)).toThrowError(
-        /not compatible with CAIP-5/i,
+      expect(() => Caip5.decode("cosmos:wonder🧝‍♂️" as ChainId)).toThrowError(/not compatible with CAIP-5/i);
+    });
+
+    it("throws for hashed chain IDs", () => {
+      expect(() => Caip5.decode("cosmos:hashed-" as ChainId)).toThrowError(
+        /hashed chain IDs cannot be decoded/i,
+      );
+      expect(() => Caip5.decode("cosmos:hashed-abab" as ChainId)).toThrowError(
+        /hashed chain IDs cannot be decoded/i,
+      );
+      expect(() => Caip5.decode("cosmos:hashed-6abb36860ec76c5a" as ChainId)).toThrowError(
+        /hashed chain IDs cannot be decoded/i,
       );
     });
   });
--- packages/iov-cosmos/src/caip5.ts
+++ packages/iov-cosmos/src/caip5.ts
@@ -1,4 +1,10 @@
 import { ChainId } from "@iov/bcp";
+import { Sha256 } from "@iov/crypto";
+import { Encoding } from "@iov/encoding";
+
+const { toHex, toUtf8 } = Encoding;
+
+const hashedPrefix = "hashed-";
 
 /**
  * Conversion between native chain IDs and CAIP-5 format
@@ -7,10 +13,12 @@ import { ChainId } from "@iov/bcp";
  */
 export class Caip5 {
   public static encode(native: string): ChainId {
-    if (!native.match(/^[-a-zA-Z0-9]{3,47}$/)) {
-      throw new Error("Given chain ID cannot be CAIP-5 encoded");
+    if (!native.match(/^[-a-zA-Z0-9]{3,47}$/) || native.startsWith(hashedPrefix)) {
+      const hash = toHex(new Sha256(toUtf8(native)).digest()).slice(0, 16);
+      return `cosmos:${hashedPrefix}${hash}` as ChainId;
+    } else {
+      return `cosmos:${native}` as ChainId;
     }
-    return `cosmos:${native}` as ChainId;
   }
 
   public static decode(chainId: ChainId): string {
@@ -18,6 +26,12 @@ export class Caip5 {
     if (!match) {
       throw new Error("Chain ID not compatible with CAIP-5");
     }
-    return match[1];
+
+    const reference = match[1];
+    if (reference.startsWith(hashedPrefix)) {
+      throw new Error("Hashed chain IDs cannot be decoded");
+    }
+
+    return reference;
   }
 }
