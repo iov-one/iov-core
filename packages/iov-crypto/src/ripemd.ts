ae26f9bbc3a39fd410c8419eaf985744071db8bf
Simon Warta <simon@warta.it>
2020-05-12 10:11:32 +0200

    Use @types/ripemd160

--- packages/iov-crypto/src/ripemd.ts
+++ packages/iov-crypto/src/ripemd.ts
@@ -1,25 +1,20 @@
-import { Encoding } from "@iov/encoding";
 import RIPEMD160 from "ripemd160";
 
 import { HashFunction } from "./hash";
 
-const { toHex } = Encoding;
-
 export class Ripemd160 implements HashFunction {
   public readonly blockSize = 512 / 8;
 
-  private readonly impl: RIPEMD160;
+  private readonly impl = new RIPEMD160();
 
   public constructor(firstData?: Uint8Array) {
-    this.impl = new RIPEMD160();
-
     if (firstData) {
       this.update(firstData);
     }
   }
 
   public update(data: Uint8Array): Ripemd160 {
-    this.impl.update(toHex(data), "hex");
+    this.impl.update(Buffer.from(data));
     return this;
   }
 
